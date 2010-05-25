d59df229b6d9adbfda8839e0f47eb53a46ac82b9
nakdai <newvf91.brjtn579.dn@gmail.com>
2010-05-25 05:52:47 +0000

    Preparation for OpenGL
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@8 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/graph/GraphicsDeviceProxy.h
+++ Libs/include/graph/GraphicsDeviceProxy.h
@@ -1,6 +1,9 @@
 #if !defined(__URANUS_GRAPH_GRAPHICS_DEVICE_PROXY_H__)
 #define __URANUS_GRAPH_GRAPHICS_DEVICE_PROXY_H__
 
-#include "graph/dx9/GraphicsDevice.h"
+#if defined(__UN_DX9__)
+	#include "graph/dx9/GraphicsDevice.h"
+#elif defined(__UN_OGL__)
+#endif
 
 #endif	// #if !defined(__URANUS_GRAPH_GRAPHICS_DEVICE_PROXY_H__)
--- Libs/include/unGraph.h
+++ Libs/include/unGraph.h
@@ -1,24 +1,27 @@
 #if !defined(__URANUS_GRAPH_H__)
 #define __URANUS_GRAPH_H__
 
-#include "unD3DDefs.h"
+#if defined(__UN_DX9__)
+	#include "unD3DDefs.h"
+
+	#include "graph/dx9/GraphicsDevice.h"
+	#include "graph/dx9/BaseTexture.h"
+	#include "graph/dx9/Texture.h"
+	#include "graph/dx9/CubeTexture.h"
+	#include "graph/dx9/Surface.h"
+	#include "graph/dx9/VertexBuffer.h"
+	#include "graph/dx9/IndexBuffer.h"
+	#include "graph/dx9/VertexShader.h"
+	#include "graph/dx9/PixelShader.h"
+	#include "graph/dx9/VertexDeclaration.h"
+
+	#include "graph/dx9/GraphPerf.h"
+
+	#include "graph/dx9/FxShader.h"
+#elif defined(__UN_OGL__)
+#endif
 
 #include "graph/GraphDefs.h"
 #include "graph/GraphicsDeviceUtil.h"
-
-#include "graph/dx9/GraphicsDevice.h"
-#include "graph/dx9/BaseTexture.h"
-#include "graph/dx9/Texture.h"
-#include "graph/dx9/CubeTexture.h"
-#include "graph/dx9/Surface.h"
-#include "graph/dx9/VertexBuffer.h"
-#include "graph/dx9/IndexBuffer.h"
-#include "graph/dx9/VertexShader.h"
-#include "graph/dx9/PixelShader.h"
-#include "graph/dx9/VertexDeclaration.h"
-
-#include "graph/dx9/GraphPerf.h"
-
-#include "graph/dx9/FxShader.h"
 
 #endif	// #if !defined(__URANUS_GRAPH_H__)
