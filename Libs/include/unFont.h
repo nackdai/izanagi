dfdb5cae8d3bf61adf1b2c0ff62761bd06e12bc5
nakdai <newvf91.brjtn579.dn@gmail.com>
2011-03-27 07:48:08 +0000

    Reset all.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@12 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/unFont.h
+++
@@ -1,8 +0,0 @@
-#if !defined(__URANUS_FONT_H__)
-#define __URANUS_FONT_H__
-
-#include "font/FontDefs.h"
-#include "font/FNTFormat.h"
-#include "font/FontRenderer.h"
-
-#endif	// #if !defined(__URANUS_FONT_H__)
