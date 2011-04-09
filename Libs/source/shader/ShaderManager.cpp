c9bddca70b280ac14e39d8283169b3aa4eda6208
newvf91.brjtn579.dn <newvf91.brjtn579.dn@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2011-04-09 17:00:50 +0000

    Add ShaderManager.cpp/h
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@22 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Libs/source/shader/ShaderManager.cpp
@@ -0,0 +1,149 @@
+﻿#include "shader/ShaderManager.h"
+
+using namespace izanagi;
+
+/**
+* Create instance of CShaderManager.
+*/
+CShaderManager* CShaderManager::CreateShaderManager(IMemoryAllocator* pAllocator)
+{
+	IZ_ASSERT(pAllocator != IZ_NULL);
+
+	void* pBuf = ALLOC(pAllocator, sizeof(CShaderManager));
+	VRETURN_NULL(pBuf != IZ_NULL);
+
+	CShaderManager* pInstance = new(pBuf) CShaderManager;
+	{
+		pInstance->AddRef();
+		pInstance->m_pAllocator = pAllocator;
+	}
+
+	return pInstance;
+}
+
+CShaderManager::CShaderManager()
+{
+	m_pAllocator = IZ_NULL;
+}
+
+CShaderManager::~CShaderManager()
+{
+	ClearAll();
+}
+
+IZ_BOOL CShaderManager::Add(CShader* pShader)
+{
+	IZ_ASSERT(pShader != IZ_NULL);
+
+	IZ_BOOL ret = m_ShaderHash.Add(pShader->GetHashItem());
+
+	if (ret) {
+		pShader->AddRef();
+	}
+
+	return ret;
+}
+
+void CShaderManager::ClearAll()
+{
+	CStdList<CShaderHashItem>::Item* pOrderItem = m_ShaderHash.GetOrderTop();
+
+	while (pOrderItem != IZ_NULL) {
+		CShaderHashItem* pHashItem = pOrderItem->GetData();
+		IZ_ASSERT(pHashItem != IZ_NULL);
+
+		CShader* pShader = pHashItem->GetData();
+		SAFE_RELEASE(pShader);
+
+		pOrderItem = pOrderItem->GetNext();
+	}
+
+	m_ShaderHash.Clear();
+}
+
+IZ_UINT CShaderManager::GetShaderNumByName(IZ_PCSTR name)
+{
+	IZ_UINT ret = GetShaderNumByKey(CKey::GenerateValue(name));
+	return ret;
+}
+
+IZ_UINT CShaderManager::GetShaderNumByKey(IZ_UINT key)
+{
+	IZ_UINT ret = 0;
+
+	CStdList<CShaderHashItem>::Item* pItem = m_ShaderHash.GetHashTop(key);
+
+	while (pItem != IZ_NULL) {
+		CShader* pShader = pItem->GetData()->GetData();
+		IZ_ASSERT(pShader != IZ_NULL);
+
+		if (pShader->GetKey() == key) {
+			ret++;
+		}
+
+		pItem = pItem->GetNext();
+	}
+
+	return ret;
+}
+
+CShader* CShaderManager::GetShaderByName(IZ_PCSTR name)
+{
+	CShader* ret = GetShaderByKey(CKey::GenerateValue(name));
+	return ret;
+}
+
+CShader* CShaderManager::GetShaderByKey(IZ_UINT key)
+{
+	// Return shader which is found firstly.
+	CShader* ret = m_ShaderHash.FindData(key);
+	return ret;
+}
+
+CShader* CShaderManager::GetShaderByAttr(
+	IZ_PCSTR name,
+	const CShaderAttr pAttrs[],
+	IZ_UINT nAttrNum)
+{
+	CShader* ret = GetShaderByAttr(
+					CKey::GenerateValue(name),
+					pAttrs,
+					nAttrNum);
+	return ret;
+}
+
+CShader* CShaderManager::GetShaderByAttr(
+	IZ_UINT key,
+	const CShaderAttr pAttrs[],
+	IZ_UINT nAttrNum)
+{
+	CStdList<CShaderHashItem>::Item* pItem = m_ShaderHash.GetHashTop(key);
+
+	while (pItem != IZ_NULL) {
+		CShader* pShader = pItem->GetData()->GetData();
+		IZ_ASSERT(pShader != IZ_NULL);
+
+		if (pShader->GetKey() == key) {
+			IZ_BOOL bIsSame = IZ_TRUE;
+
+			for (IZ_UINT i = 0; i < nAttrNum; i++) {
+				const CShaderAttr& attr = pAttrs[i];
+
+				if (!pShader->CmpAttr(attr)) {
+					// Not match attribute.
+					bIsSame = IZ_FALSE;
+					break;
+				}
+			}
+
+			if (bIsSame) {
+				// Find shader.
+				return pShader;
+			}
+		}
+
+		pItem = pItem->GetNext();
+	}
+
+	return IZ_NULL;
+}
