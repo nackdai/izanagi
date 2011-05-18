30c8a365538be87a328bea9e66dfac1a06979a06
nakdai <newvf91.brjtn579.dn@gmail.com>
2011-05-18 15:02:11 +0000

    Modify E_SHADER_PARAMETER_TYPE, E_POSTEFFECT_PARAMETER_TYPE.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@35 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/posteffect/util/PostEffectUtilityParam.h
+++ Libs/include/posteffect/util/PostEffectUtilityParam.h
@@ -14,38 +14,92 @@ namespace izanagi {
 		~CPostEffectParamUtil();
 
 	public:
-		// パラメータタイプから行数を取得
-		static IZ_UINT GetParamRowFromParamType(E_POSTEFFECT_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプから行数を取得
+		 *
+		 * 例えば
+		 *    E_POSTEFFECT_PARAMETER_TYPE_FLOAT3x4 -> 3
+		 *    E_POSTEFFECT_PARAMETER_TYPE_FLOAT    -> 1
+		 *    E_POSTEFFECT_PARAMETER_TYPE_FLOAT4   -> 1
+		 * が返ってくる
+		 */
+		static inline IZ_UINT GetParamRowFromParamType(E_POSTEFFECT_PARAMETER_TYPE type);
 
-		// パラメータタイプから列数を取得
-		static IZ_UINT GetParamColumnFromParamType(E_POSTEFFECT_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプから列数を取得
+		 *
+		 * 例えば
+		 *    E_POSTEFFECT_PARAMETER_TYPE_FLOAT3x4 -> 4
+		 *    E_POSTEFFECT_PARAMETER_TYPE_FLOAT    -> 1
+		 *    E_POSTEFFECT_PARAMETER_TYPE_FLOAT4   -> 4
+		 * が返ってくる
+		 */
+		static inline IZ_UINT GetParamColumnFromParamType(E_POSTEFFECT_PARAMETER_TYPE type);
 
-		// パラメータタイプがintかどうか
-		static IZ_BOOL IsIntType(E_POSTEFFECT_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプがintかどうか
+		 */
+		static inline IZ_BOOL IsIntType(E_POSTEFFECT_PARAMETER_TYPE type);
 
-		// パラメータタイプがfloatかどうか
-		static IZ_BOOL IsFloatType(E_POSTEFFECT_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプがfloatかどうか
+		 */
+		static inline IZ_BOOL IsFloatType(E_POSTEFFECT_PARAMETER_TYPE type);
 
 	public:
-		// パラメータタイプがboolかどうか
+		/**
+		 * パラメータタイプがboolかどうか
+		 */
 		static inline IZ_BOOL IsBoolType(E_POSTEFFECT_PARAMETER_TYPE type);
 
-		// パラメータタイプがmatrix(=float4x4)かどうか
+		/**
+		 * パラメータタイプがmatrix(=float4x4)かどうか
+		 */
 		static inline IZ_BOOL IsMatrixType(E_POSTEFFECT_PARAMETER_TYPE type);
 
 	public:
-		// パラメータタイプからバイトサイズを取得する
+		/**
+		 * パラメータタイプからバイトサイズを取得する
+		 */ 
 		static inline IZ_UINT GetBytesFromParamType(E_POSTEFFECT_PARAMETER_TYPE type);
 
-		// パラメータタイプがテクスチャかどうか
+		/**
+		 * パラメータタイプがテクスチャかどうか
+		 */
 		static inline IZ_BOOL IsTextureType(E_POSTEFFECT_PARAMETER_TYPE type);
 	};
 
 	// inline ******************************
 
-	/**
-	* パラメータタイプがboolかどうか
-	*/
+	// パラメータタイプから行数を取得
+	IZ_UINT CPostEffectParamUtil::GetParamRowFromParamType(E_POSTEFFECT_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_GET_ROW_POSTEFFECT_PARAM_TYPE(type);
+		return ret;
+	}
+
+	// パラメータタイプから列数を取得
+	IZ_UINT CPostEffectParamUtil::GetParamColumnFromParamType(E_POSTEFFECT_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_GET_COLUMN_POSTEFFECT_PARAM_TYPE(type);
+		return ret;
+	}
+
+	// パラメータタイプがintかどうか
+	IZ_BOOL CPostEffectParamUtil::IsIntType(E_POSTEFFECT_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_IS_INT_POSTEFFECT_PARAM_TYPE(type);
+		return (ret == 1);
+	}
+
+	// パラメータタイプがfloatかどうか
+	IZ_BOOL CPostEffectParamUtil::IsFloatType(E_POSTEFFECT_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_IS_FLOAT_POSTEFFECT_PARAM_TYPE(type);
+		return (ret == 1);
+	}
+
+	// パラメータタイプがboolかどうか
 	IZ_BOOL CPostEffectParamUtil::IsBoolType(E_POSTEFFECT_PARAMETER_TYPE type)
 	{
 		IZ_BOOL ret = ((E_POSTEFFECT_PARAMETER_TYPE_BOOL <= type)
@@ -53,18 +107,14 @@ namespace izanagi {
 		return ret;
 	}
 
-	/**
-	* パラメータタイプがmatrix(=float4x4)かどうか
-	*/
+	// パラメータタイプがmatrix(=float4x4)かどうか
 	IZ_BOOL CPostEffectParamUtil::IsMatrixType(E_POSTEFFECT_PARAMETER_TYPE type)
 	{
 		IZ_BOOL ret = (type == E_POSTEFFECT_PARAMETER_TYPE_FLOAT4x4);
 		return ret;
 	}
 
-	/**
-	* パラメータタイプからバイトサイズを取得する
-	*/
+	// パラメータタイプからバイトサイズを取得する
 	IZ_UINT CPostEffectParamUtil::GetBytesFromParamType(E_POSTEFFECT_PARAMETER_TYPE type)
 	{
 		IZ_UINT nRow = GetParamRowFromParamType(type);			// 行数
@@ -82,9 +132,7 @@ namespace izanagi {
 		return ret;
 	}
 
-	/**
-	* パラメータタイプがテクスチャかどうか
-	*/
+	// パラメータタイプがテクスチャかどうか
 	IZ_BOOL CPostEffectParamUtil::IsTextureType(E_POSTEFFECT_PARAMETER_TYPE type)
 	{
 		return (type == E_POSTEFFECT_PARAMETER_TYPE_TEXTURE);
--- Libs/include/shader/base/util/ShaderParamUtil.h
+++ Libs/include/shader/base/util/ShaderParamUtil.h
@@ -14,38 +14,92 @@ namespace izanagi {
 		~CShaderParamUtil();
 
 	public:
-		// パラメータタイプから行数を取得
-		static IZ_UINT GetParamRowFromParamType(E_SHADER_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプから行数を取得
+		 *
+		 * 例えば
+		 *    E_SHADER_PARAMETER_TYPE_FLOAT3x4 -> 3
+		 *    E_SHADER_PARAMETER_TYPE_FLOAT    -> 1
+		 *    E_SHADER_PARAMETER_TYPE_FLOAT4   -> 1
+		 * が返ってくる
+		 */
+		static inline IZ_UINT GetParamRowFromParamType(E_SHADER_PARAMETER_TYPE type);
 
-		// パラメータタイプから列数を取得
-		static IZ_UINT GetParamColumnFromParamType(E_SHADER_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプから列数を取得
+		 *
+		 * 例えば
+		 *    E_SHADER_PARAMETER_TYPE_FLOAT3x4 -> 4
+		 *    E_SHADER_PARAMETER_TYPE_FLOAT    -> 1
+		 *    E_SHADER_PARAMETER_TYPE_FLOAT4   -> 4
+		 * が返ってくる
+		 */
+		static inline IZ_UINT GetParamColumnFromParamType(E_SHADER_PARAMETER_TYPE type);
 
-		// パラメータタイプがintかどうか
-		static IZ_BOOL IsIntType(E_SHADER_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプがintかどうか
+		 */
+		static inline IZ_BOOL IsIntType(E_SHADER_PARAMETER_TYPE type);
 
-		// パラメータタイプがfloatかどうか
-		static IZ_BOOL IsFloatType(E_SHADER_PARAMETER_TYPE type);
+		/**
+		 * パラメータタイプがfloatかどうか
+		 */
+		static inline IZ_BOOL IsFloatType(E_SHADER_PARAMETER_TYPE type);
 
 	public:
-		// パラメータタイプがboolかどうか
+		/**
+		 * パラメータタイプがboolかどうか
+		 */
 		static inline IZ_BOOL IsBoolType(E_SHADER_PARAMETER_TYPE type);
 
-		// パラメータタイプがmatrix(=float4x4)かどうか
+		/**
+		 * パラメータタイプがmatrix(=float4x4)かどうか
+		 */
 		static inline IZ_BOOL IsMatrixType(E_SHADER_PARAMETER_TYPE type);
 
 	public:
-		// パラメータタイプからバイトサイズを取得する
+		/**
+		 * パラメータタイプからバイトサイズを取得する
+		 */
 		static inline IZ_UINT GetBytesFromParamType(E_SHADER_PARAMETER_TYPE type);
 
-		// パラメータタイプがテクスチャかどうか
+		/**
+		 * パラメータタイプがテクスチャかどうか
+		 */
 		static inline IZ_BOOL IsTextureType(E_SHADER_PARAMETER_TYPE type);
 	};
 
 	// inline ******************************
 
-	/**
-	* パラメータタイプがboolかどうか
-	*/
+	// パラメータタイプから行数を取得
+	IZ_UINT CShaderParamUtil::GetParamRowFromParamType(E_SHADER_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_GET_ROW_SHADER_PARAM_TYPE(type);
+		return ret;
+	}
+
+	// パラメータタイプから列数を取得
+	IZ_UINT CShaderParamUtil::GetParamColumnFromParamType(E_SHADER_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_GET_COLUMN_SHADER_PARAM_TYPE(type);
+		return ret;
+	}
+
+	// パラメータタイプがintかどうか
+	IZ_BOOL CShaderParamUtil::IsIntType(E_SHADER_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_IS_INT_SHADER_PARAM_TYPE(type);
+		return (ret == 1);
+	}
+
+	// パラメータタイプがfloatかどうか
+	IZ_BOOL CShaderParamUtil::IsFloatType(E_SHADER_PARAMETER_TYPE type)
+	{
+		IZ_UINT ret = IZ_IS_FLOAT_SHADER_PARAM_TYPE(type);
+		return (ret == 1);
+	}
+
+	// パラメータタイプがboolかどうか
 	IZ_BOOL CShaderParamUtil::IsBoolType(E_SHADER_PARAMETER_TYPE type)
 	{
 		IZ_BOOL ret = ((E_SHADER_PARAMETER_TYPE_BOOL <= type)
@@ -53,18 +107,14 @@ namespace izanagi {
 		return ret;
 	}
 
-	/**
-	* パラメータタイプがmatrix(=float4x4)かどうか
-	*/
+	// パラメータタイプがmatrix(=float4x4)かどうか
 	IZ_BOOL CShaderParamUtil::IsMatrixType(E_SHADER_PARAMETER_TYPE type)
 	{
 		IZ_BOOL ret = (type == E_SHADER_PARAMETER_TYPE_FLOAT4x4);
 		return ret;
 	}
 
-	/**
-	* パラメータタイプからバイトサイズを取得する
-	*/
+	// パラメータタイプからバイトサイズを取得する
 	IZ_UINT CShaderParamUtil::GetBytesFromParamType(E_SHADER_PARAMETER_TYPE type)
 	{
 		IZ_UINT nRow = GetParamRowFromParamType(type);			// 行数
@@ -82,9 +132,7 @@ namespace izanagi {
 		return ret;
 	}
 
-	/**
-	* パラメータタイプがテクスチャかどうか
-	*/
+	// パラメータタイプがテクスチャかどうか
 	IZ_BOOL CShaderParamUtil::IsTextureType(E_SHADER_PARAMETER_TYPE type)
 	{
 		return (type == E_SHADER_PARAMETER_TYPE_TEXTURE);
