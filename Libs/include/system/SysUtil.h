66c8e7690d6cefc871782f1c0f28a686abd8b09b
newvf91.brjtn579.dn <newvf91.brjtn579.dn@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2011-10-20 15:36:49 +0000

    - Added CSysUtil.  - CSysUtil has a API which changes current directory to directory which is same to exe path. - Modify argument of CSysDisplay::Init
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@106 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Libs/include/system/SysUtil.h
@@ -0,0 +1,23 @@
+﻿#if !defined(__IZANAGI_SYSTEM_SYS_UTIL_H__)
+#define __IZANAGI_SYSTEM_SYS_UTIL_H__
+
+#include "izDefs.h"
+
+namespace izanagi {
+	/**
+	*/
+	class CSysUtil {
+	private:
+		CSysUtil();
+		~CSysUtil();
+
+		NO_COPIABLE(CSysUtil);
+
+	public:
+		/** 実行プログラムの位置からカレントディレクトリを設定.
+		 */
+		static IZ_BOOL SetCurrentDirectoryFromExe();
+	};
+}	// namespace izanagi
+
+#endif	// #if !defined(__IZANAGI_SYSTEM_SYS_UTIL_H__)
---
+++ Libs/source/system/SysUtil.cpp
@@ -0,0 +1,32 @@
+﻿#include <windows.h>
+#include <shlwapi.h>
+#include "system/SysUtil.h"
+
+#pragma comment(lib, "shlwapi.lib")
+
+using namespace izanagi;
+
+// 実行プログラムの位置からカレントディレクトリを設定.
+IZ_BOOL CSysUtil::SetCurrentDirectoryFromExe()
+{
+	static izChar buf[_MAX_PATH];
+
+	// 実行プログラムのフルパスを取得
+	{
+		DWORD result = ::GetModuleFileName(
+						IZ_NULL,
+						buf,
+						sizeof(buf));
+		VRETURN(result > 0);
+	}
+
+	// ファイル名を取り除く
+	BOOL result = ::PathRemoveFileSpec(buf);
+	VRETURN(result);
+
+	// カレントディレクトリを設定
+	result = ::SetCurrentDirectory(buf);
+	VRETURN(result);
+
+	return result;
+}
