c214e05fe6c34e21b40aed95eccd07544694585a
newvf91.brjtn579.dn <newvf91.brjtn579.dn@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2011-11-20 16:20:00 +0000

    - Commit SysMonitor temporary. - Commit StdQueue.h temporary. - Refactoring SysCondVar, StdList.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@122 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Libs/include/system/SysMonitor.h
@@ -0,0 +1,47 @@
+﻿#if !defined(__IZANAGI_SYSTEM_SYS_MONITOR_H__)
+#define __IZANAGI_SYSTEM_SYS_MONITOR_H__
+
+#include "izDefs.h"
+#include "SysThreadDefs.h"
+
+namespace izanagi {
+	/**
+	 */
+	class CMonitor {
+		friend class CSyncronized;
+
+	public:
+		CMonitor();
+		~CMonitor();
+
+		NO_COPIABLE(CMonitor);
+
+	private:
+		// syncronized開始.
+		void BeginSyncronized();
+
+		// syncronized終了.
+		IZ_BOOL EndSyncronized();
+
+	public:
+		/** 待機.
+		 */
+		void Wait();
+
+		/** 待機中のスレッドを１つ起こす.
+		 */
+		void Notify();
+
+		/** 待機中のスレッドを全て起こす.
+		 */
+		void NotifyAll();
+
+	private:
+		class CMonitorImpl;
+
+		CMonitorImpl* m_Impl;
+		IZ_UINT8 m_Buf[1];	// TODO
+	};
+}	// namespace izanagi
+
+#endif	// #if !defined(__IZANAGI_SYSTEM_SYS_MONITOR_H__)
