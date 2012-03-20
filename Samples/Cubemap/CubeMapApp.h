b1971c80d25af46dd6cd0a38bf88a568e2ab38a2
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-03-20 17:19:19 +0000

    Add CubeMap sample. But it does not render correctly.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@220 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Samples/Cubemap/CubeMapApp.h
@@ -0,0 +1,38 @@
+#if !defined(__CUBE_MAP_APP_H__)
+#define __CUBE_MAP_APP_H__
+
+#include "izSampleKit.h"
+
+static const IZ_UINT SCREEN_WIDTH = 1280;
+static const IZ_UINT SCREEN_HEIGHT = 720;
+
+class CCubeMapApp: public izanagi::sample::CSampleApp {
+public:
+	CCubeMapApp();
+	virtual ~CCubeMapApp();
+
+protected:
+	// 初期化.
+	virtual IZ_BOOL InitInternal(
+		izanagi::IMemoryAllocator* allocator,
+		izanagi::CGraphicsDevice* device,
+		izanagi::sample::CSampleCamera& camera);
+
+	// 解放.
+	virtual void ReleaseInternal();
+
+	// 更新.
+	virtual void UpdateInternal(izanagi::CCamera& camera);
+
+	// 描画.
+	virtual void RenderInternal(izanagi::CGraphicsDevice* device);
+
+private:
+	izanagi::CDebugMesh* m_Cube;
+	izanagi::CImage* m_Img;
+	izanagi::CShaderBasic* m_Shader;
+
+	izanagi::SMatrix m_L2W;
+};
+
+#endif	// #if !defined(__CUBE_MAP_APP_H__)
