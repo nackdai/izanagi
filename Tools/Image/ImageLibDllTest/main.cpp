42c4d5a37016ab2c451ce90dd4c026b1fb40dea9
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-04-03 09:45:28 +0000

    Add ImageLibDllTest.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@241 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Tools/Image/ImageLibDllTest/main.cpp
@@ -0,0 +1,32 @@
+// ImageBuilder.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#ifndef _WIN32_WINNT		// Windows XP 以降のバージョンに固有の機能の使用を許可します。                   
+#define _WIN32_WINNT 0x0501	// これを Windows の他のバージョン向けに適切な値に変更してください。
+#endif						
+
+#include "ImageLibDll.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+#ifdef VGOTO
+#undef VGOTO
+#endif
+
+#define VGOTO(b)	if (!(b)) { IZ_ASSERT(FALSE); nRetCode = 1; goto __EXIT__; }
+
+int main(int argc, char* argv[])
+{
+	int nRetCode = 0;
+
+	HWND hWnd = ::GetConsoleWindow();
+	VGOTO(hWnd != IZ_NULL);
+
+	// イメージライブラリ初期化
+	IZ_BOOL result = izInitImageLib(hWnd);
+
+__EXIT__:
+	return nRetCode;
+}
