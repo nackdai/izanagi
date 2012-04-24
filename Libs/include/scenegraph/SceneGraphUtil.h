b23f269917caec5b215050956b3f7c77892465a4
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-04-24 14:59:53 +0000

    - Fix some wrong computing.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@243 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/scenegraph/SceneGraphUtil.h
+++ Libs/include/scenegraph/SceneGraphUtil.h
@@ -19,25 +19,23 @@ namespace izanagi {
 		// Clip - Screen 座標変換マトリクス計算
 		static void ComputeC2S(
 			SMatrix& mtxC2S,
-			IZ_FLOAT fScreenWidth,
-			IZ_FLOAT fScreenHeight,
-			IZ_FLOAT fCenterX,
-			IZ_FLOAT fCenterY,
-			IZ_FLOAT fMinZ,
-			IZ_FLOAT fMaxZ);
+			IZ_INT screenWidth,
+			IZ_INT screenHeight,
+			IZ_FLOAT minZ,
+			IZ_FLOAT maxZ);
 
 		// スクリーン距離計算
 		static IZ_FLOAT ComputeScreenDistance(
-			IZ_FLOAT fScreenWidth,
-			IZ_FLOAT fFov);
+			IZ_FLOAT screenHeight,
+			IZ_FLOAT fov);
 
 		// クリップ座標取得
 		static void Screen2Clip(
 			SVector& vClip,
 			const SCameraParam& sCamera,
 			const SViewport& vp,
-			IZ_INT nX,
-			IZ_INT nY);
+			IZ_INT nX, IZ_INT nY,
+			IZ_FLOAT fZ);
 
 		// 光線を取得
 		static void Point2Ray(
