e3a1adb014f263ebaeb488a4540cf4e59db8386f
newvf91.brjtn579.dn <newvf91.brjtn579.dn@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2012-04-24 14:59:53 +0000

    - Fix some wrong computing.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@243 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/source/scenegraph/SceneGraphUtil.cpp
+++ Libs/source/scenegraph/SceneGraphUtil.cpp
@@ -5,33 +5,31 @@ using namespace izanagi;
 // Clip - Screen 座標変換マトリクス計算
 void CSceneGraphUtil::ComputeC2S(
 	SMatrix& mtxC2S,
-	IZ_FLOAT fScreenWidth,
-	IZ_FLOAT fScreenHeight,
-	IZ_FLOAT fCenterX,
-	IZ_FLOAT fCenterY,
-	IZ_FLOAT fMinZ,
-	IZ_FLOAT fMaxZ)
+	IZ_INT screenWidth,
+	IZ_INT screenHeight,
+	IZ_FLOAT minZ,
+	IZ_FLOAT maxZ)
 {
 	SMatrix::SetUnit(mtxC2S);
 
-	mtxC2S.m[0][0] = fScreenWidth / 2.0f;
-	mtxC2S.m[1][1] = -fScreenHeight / 2.0f;
+	mtxC2S.m[0][0] = screenWidth * 0.5f;;
+	mtxC2S.m[1][1] = -screenHeight * 0.5f;;
 
-	mtxC2S.m[2][2] = fMaxZ - fMinZ;
-	mtxC2S.m[3][2] = fMinZ;
+	mtxC2S.m[2][2] = maxZ - minZ;
+	mtxC2S.m[3][2] = minZ;
 
-	mtxC2S.m[3][0] = fCenterX;
-	mtxC2S.m[3][1] = fCenterY;
+	mtxC2S.m[3][0] = screenWidth * 0.5f;;
+	mtxC2S.m[3][1] = screenHeight * 0.5f;
 }
 
 // スクリーン距離計算
 IZ_FLOAT CSceneGraphUtil::ComputeScreenDistance(
-	IZ_FLOAT fScreenWidth,
-	IZ_FLOAT fFov)
+	IZ_FLOAT screenHeight,
+	IZ_FLOAT fov)
 {
 	// NOTE
-	// (W/2)/S = tan(θ/2) <=> S = (W/2)/tan(θ/2)
-	IZ_FLOAT ret = fScreenWidth * 0.5f / tanf(fFov * 0.5f);
+	// (H/2)/S = tan(θ/2) <=> S = (H/2)/tan(θ/2)
+	IZ_FLOAT ret = screenHeight * 0.5f / tanf(fov * 0.5f);
 	return ret;
 }
 
@@ -40,8 +38,8 @@ void CSceneGraphUtil::Screen2Clip(
 	SVector& vClip,
 	const SCameraParam& camera,
 	const SViewport& vp,
-	IZ_INT nX,
-	IZ_INT nY)
+	IZ_INT nX, IZ_INT nY,
+	IZ_FLOAT fZ)
 {
 	IZ_INT nW = vp.width;
 	IZ_INT nH = vp.height;
@@ -51,7 +49,7 @@ void CSceneGraphUtil::Screen2Clip(
 	vClip.Set(
 		2.0f * (nX - nCx) / nW,
 		-2.0f * (nY - nCy) / nH,
-		0.0f,
+		(fZ - vp.minZ) / (vp.maxZ - vp.minZ),
 		1.0f);
 }
 
@@ -64,15 +62,20 @@ void CSceneGraphUtil::Point2Ray(
 	IZ_INT nY)
 {
 #if 0
+	SMatrix mtxS2C;
+	ComputeC2S(
+		mtxS2C,
+		vp.width, vp.height,
+		vp.minZ, vp.maxZ);
+	SMatrix::Inverse(mtxS2C, mtxS2C);
+
 	SVector clip;
 
 	// Screen -> Clip
-	Screen2Clip(
+	SMatrix::Apply(
 		clip,
-		camera,
-		vp,
-		vp.width >> 1,
-		vp.height >> 1);
+		CVector((IZ_FLOAT)nX, (IZ_FLOAT)nY, 1.0f, 1.0f),
+		mtxS2C);
 
 	SMatrix mtxC2V;
 	SMatrix::Inverse(mtxC2V, camera.mtxV2C);
@@ -81,8 +84,35 @@ void CSceneGraphUtil::Point2Ray(
 	SMatrix::Inverse(mtxV2W, camera.mtxW2V);
 
 	SVector rayFar;
+	{
+		// Clip -> View
+		izanagi::SMatrix::Apply(rayFar, clip, mtxC2V);
 
-	clip.z = vp.maxZ;
+		// View -> World
+		izanagi::SMatrix::Apply(rayFar, rayFar, mtxV2W);
+
+		izanagi::SVector::Div(rayFar, rayFar, rayFar.w);
+	}
+
+	izanagi::SVector::Sub(ray, rayFar, camera.pos);
+#elif 0
+	SVector clip;
+
+	// Screen -> Clip
+	Screen2Clip(
+		clip,
+		camera,
+		vp,
+		nX, nY,
+		1.0f);
+
+	SMatrix mtxC2V;
+	SMatrix::Inverse(mtxC2V, camera.mtxV2C);
+
+	SMatrix mtxV2W;
+	SMatrix::Inverse(mtxV2W, camera.mtxW2V);
+
+	SVector rayFar;
 	{
 		// Clip -> View
 		izanagi::SMatrix::Apply(rayFar, clip, mtxC2V);
@@ -98,19 +128,22 @@ void CSceneGraphUtil::Point2Ray(
 	// NOTE
 	//
 	// Clip->Screen変換
-	// | W/2  0    0 0 |
-	// |  0 -H/2   0 0 |
-	// |  0   0    1 0 |
-	// | W/2 H/2  -1 1 |
+	// | W/2  0        0       0 |
+	// |  0 -H/2       0       0 |
+	// |  0   0   maxZ - minZ  0 |
+	// | W/2 H/2     -minZ     1 |
 	//
 	// 逆行列
 	// | 1/(W/2)   0      0 0 |
 	// |  0      -1/(H/2) 0 0 |
-	// |  0        0      1 0 |
-	// | -1        1      0 1 |
+	// |  0        0      a 0 |
+	// | -1        1      b 1 |
+	//
+	// a = 1 / (maxZ - miinZ)
+	// b = -minZ / (maxZ - miinZ)
 	//
 	// ここから、(x, y, z, 1)のスクリーン座標をClip座標に変換すると
-	// (x/(W/2) - 1, -y/(H/2) + 1, z, 1)
+	// (x/(W/2) - 1, -y/(H/2) + 1, (z - minZ) / (maxZ - minZ), 1)
 	//
 	// Scree->View変換の逆行列
 	// | 1/Sx  0   0    0     |
@@ -123,26 +156,30 @@ void CSceneGraphUtil::Point2Ray(
 	// Sz = F/(F - N)
 	//
 	// ここにClip座標に変換した座標をかけると
-	//  2a(x - W/2)       -2(y - H/2)           -z(F - N) + F
+	//  2a(x - W/2)       -2(y - H/2)           -Z(F - N) + F
 	// (-----------tanθ, ------------tanθ, 1, -------------)
-	//       W                 H                     FN
+	//       W                 H                      FN
+	//
+	// Z = (z - minZ) / (maxZ - minZ)
+	//
 	// となる
 
-	IZ_INT nW = vp.width;
-	IZ_INT nH = vp.height;
-	IZ_INT nCx = nW >> 1;
-	IZ_INT nCy = nH >> 1;
+	const IZ_INT nW = vp.width;
+	const IZ_INT nH = vp.height;
+	const IZ_INT nCx = nW >> 1;
+	const IZ_INT nCy = nH >> 1;
 
-	IZ_FLOAT fTan = tanf(camera.fov * 0.5f);
-	const IZ_FLOAT& fAspect = camera.aspect;
+	const IZ_FLOAT fTan = tanf(camera.fov * 0.5f);
+	const IZ_FLOAT fAspect = camera.aspect;
 
 	// Scree -> Clip
 	IZ_FLOAT x = 2.0f * fAspect * (nX - nCx) * fTan / nW;
 	IZ_FLOAT y = -2.0f * (nY - nCy) * fTan / nH;
 
-	IZ_FLOAT div = 1.0f / (camera.cameraFar * camera.cameraNear);
-	IZ_FLOAT z = vp.maxZ;
-	IZ_FLOAT w = (-z * (camera.cameraFar - camera.cameraNear) + camera.cameraFar) * div;
+	const IZ_FLOAT F = camera.cameraFar;
+	const IZ_FLOAT N = camera.cameraNear;
+	IZ_FLOAT z = (1.0f - vp.minZ) / (vp.maxZ - vp.minZ);
+	IZ_FLOAT w = (-z * (F - N) + F) / (F * N);
 
 	SMatrix mtx;
 	SMatrix::Inverse(mtx, camera.mtxW2V);
