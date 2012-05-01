c0b0a98696498daa6430a2c14746e4327561cb35
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-05-01 16:34:53 +0000

    Add Decal sample. But this commit is temporary.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@254 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Samples/Decal/DecalApp.h
@@ -0,0 +1,45 @@
+#if !defined(__DECAL_APP_H__)
+#define __DECAL_APP_H__
+
+#include "izSampleKit.h"
+#include "Sphere.h"
+
+static const IZ_UINT SCREEN_WIDTH = 1280;
+static const IZ_UINT SCREEN_HEIGHT = 720;
+
+class CDecalApp : public izanagi::sample::CSampleApp {
+public:
+	CDecalApp();
+	virtual ~CDecalApp();
+
+protected:
+	// 初期化.
+	virtual IZ_BOOL InitInternal(
+		izanagi::IMemoryAllocator* allocator,
+		izanagi::CGraphicsDevice* device,
+		izanagi::sample::CSampleCamera& camera);
+
+	// 解放.
+	virtual void ReleaseInternal();
+
+	// 更新.
+	virtual void UpdateInternal(
+		izanagi::CCamera& camera,
+		izanagi::CGraphicsDevice* device);
+
+	// 描画.
+	virtual void RenderInternal(izanagi::CGraphicsDevice* device);
+
+	virtual IZ_BOOL OnKeyDown(IZ_UINT nChar);
+	virtual void OnKeyUp(IZ_UINT nChar);
+	virtual IZ_BOOL OnMouseLBtnDown(const izanagi::CIntPoint& point);
+
+private:
+	CSphere* m_Sphere;
+
+	izanagi::CImage* m_Img;
+
+	izanagi::CShaderBasic* m_Shader;
+};
+
+#endif	// #if !defined(__DECAL_APP_H__)
