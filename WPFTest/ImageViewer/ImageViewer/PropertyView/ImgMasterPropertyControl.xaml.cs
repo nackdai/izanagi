a247d5bd31a52df15467334a9a5b2ab87bbecd74
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-05-22 15:55:31 +0000

    - Add ImageMasterPropertyControl. - It is enabled to show/hide ImageMasterPropertyControl.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@277 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ WPFTest/ImageViewer/ImageViewer/PropertyView/ImgMasterPropertyControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace ImageViewer
+{
+    /// <summary>
+    /// ImgMasterPropertyControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ImgMasterPropertyControl : UserControl
+    {
+        public ImgMasterPropertyControl()
+        {
+            InitializeComponent();
+
+            this.DataContext = new ImgMasterPropertyViewModel();
+        }
+    }
+}
---
+++ WPFTest/ImageViewer/ImageViewer/PropertyView/ImgMasterPropertyViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace ImageViewer
+{
+    class ImgMasterPropertyViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private string hoge = "hoge";
+        public string ImageType
+        {
+            get { return hoge; }
+        }
+    }
+}
