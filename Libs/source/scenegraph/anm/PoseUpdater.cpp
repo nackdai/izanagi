2e98a027f795098f259276523e63befa03a3a651
newvf91.brjtn579.dn <newvf91.brjtn579.dn@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2012-06-17 08:19:19 +0000

    Add PoseUpdater.cpp.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@292 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Libs/source/scenegraph/anm/PoseUpdater.cpp
@@ -0,0 +1,80 @@
+#include "scenegraph/anm/PoseUpdater.h"
+#include "scenegraph/skl/SkeletonInstance.h"
+#include "scenegraph/skl/SkeletonUtil.h"
+
+using namespace izanagi;
+
+CPoseUpdater::CPoseUpdater(S_SKL_JOINT_POSE* pose)
+{
+	m_Pose = pose;
+}
+
+IZ_BOOL CPoseUpdater::BeginUpdate(IZ_UINT idx)
+{
+	// Nothing is done.
+	UNUSED_ALWAYS(idx);
+	return true;
+}
+
+void CPoseUpdater::EndUpdate(IZ_UINT idx, IZ_UINT updateFlag)
+{
+	// Nothing is done.
+	UNUSED_ALWAYS(idx);
+	UNUSED_ALWAYS(updateFlag);
+}
+
+void CPoseUpdater::Update(
+	IZ_UINT idx,
+	IZ_UINT transformType,
+	IZ_UINT paramType,
+	const SVector& param)
+{
+	UNUSED_ALWAYS(idx);
+
+	IZ_ASSERT(m_Pose != IZ_NULL);
+
+	CSkeletonUtil::UpdatePose(
+		*m_Pose,
+		transformType,
+		paramType,
+		param);
+}
+
+////////////////////////////////////////////////////
+
+CSklPoseUpdater::CSklPoseUpdater(CSkeletonInstance* skl)
+{
+	m_Skl = IZ_NULL;
+	SAFE_REPLACE(m_Skl, skl);
+}
+
+CSklPoseUpdater::~CSklPoseUpdater()
+{
+	SAFE_RELEASE(m_Skl);
+}
+
+IZ_BOOL CSklPoseUpdater::BeginUpdate(IZ_UINT idx)
+{
+	IZ_ASSERT(m_Skl != IZ_NULL);
+	return m_Skl->BeginUpdatePose(idx);
+}
+
+void CSklPoseUpdater::EndUpdate(IZ_UINT idx, IZ_UINT updateFlag)
+{
+	IZ_ASSERT(m_Skl != IZ_NULL);
+	m_Skl->EndUpdatePose(idx, updateFlag);
+}
+
+void CSklPoseUpdater::Update(
+	IZ_UINT idx,
+	IZ_UINT transformType,
+	IZ_UINT paramType,
+	const SVector& param)
+{
+	IZ_ASSERT(m_Skl != IZ_NULL);
+	m_Skl->UpdatePose(
+		idx,
+		transformType,
+		paramType,
+		param);
+}
