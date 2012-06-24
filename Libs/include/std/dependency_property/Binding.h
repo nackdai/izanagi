58ba1e5f6171810bf2f30e3260e48bc35d836c65
newvf91.brjtn579.dn <newvf91.brjtn579.dn@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2012-06-24 14:35:26 +0000

    Add codes for DataBinding.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@298 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Libs/include/std/dependency_property/Binding.h
@@ -0,0 +1,112 @@
+#if !defined(__IZANAGI_STD_BINDING_H__)
+#define __IZANAGI_STD_BINDING_H__
+
+#include "izDefs.h"
+#include "std/StdObject.h"
+#include "std/StdPlacementNew.h"
+#include "std/StdString.h"
+#include "std/StdValue.h"
+#include "std/allocator/MemoryAllocator.h"
+#include "DependencyProperty.h"
+
+namespace izanagi {
+	/**
+	 */
+	enum E_BINDING_MODE
+	{
+		E_BINDING_MODE_ONE_WAY = 0,
+		E_BINDING_MODE_TWO_WAY,
+		E_BINDING_MODE_ONE_TIME,
+		E_BINDING_MODE_ONE_WAY_TO_SOURCE,
+
+		E_BINDING_MODE_NUM,
+		E_BINDING_MODE_FORCE_INT32 = 0x7fffffff,
+	};
+
+	/**
+	 */
+	class Binding : public CObject
+	{
+	public:
+		/**
+		 */
+		static IZ_UINT PropertyNameMax;
+
+		/**
+		 */
+		typedef DependencyProperty::PropertyName PropertyName;
+
+		/**
+		 */
+		template <typename T>
+		static T* CreateBinding(IMemoryAllocator* allocator, IZ_PCSTR name)
+		{
+			void* buf = ALLOC_ZERO(allocator, sizeof(T));
+			VRETURN_NULL(buf != IZ_NULL);
+
+			T* ret = new(buf) T;
+			{
+				Binding* instance = ret;
+				instance->AddRef();
+				instance->m_Allocator = allocator;
+				instance->SetPropertyName(name);
+			}
+
+			return ret;
+		}
+
+	protected:
+		Binding()
+		{
+			m_Allocator = IZ_NULL;
+			m_Mode = E_BINDING_MODE_ONE_WAY;
+		}
+		virtual ~Binding() {}
+
+		IZ_DEFINE_INTERNAL_RELEASE();
+
+	public:
+
+		/**
+		 */
+		E_BINDING_MODE GetMode()
+		{
+			return m_Mode;
+		}
+
+		/**
+		 */
+		const PropertyName& GetPropertyName()
+		{
+			return m_PropName;
+		}
+
+		/**
+		 */
+		IZ_UINT GetPropertyKey()
+		{
+			return m_Key;
+		}
+
+		/**
+		 */
+		virtual void GetValue(CValue& ret) = 0;
+
+	private:
+		void SetPropertyName(IZ_PCSTR name)
+		{
+			m_PropName.SetString(name);
+			m_Key = m_PropName.GetKeyValue();
+		}
+
+	private:
+		IMemoryAllocator* m_Allocator;
+
+		E_BINDING_MODE m_Mode;
+
+		PropertyName m_PropName;
+		IZ_UINT m_Key;		
+	};
+}	// namespace izanagi
+
+#endif	// #if !defined(__IZANAGI_STD_BINDING_H__)
---
+++ Libs/include/std/dependency_property/BindingOperations.h
@@ -0,0 +1,47 @@
+#if !defined(__IZANAGI_STD_BINDING_OPERATIONS_H__)
+#define __IZANAGI_STD_BINDING_OPERATIONS_H__
+
+#include "izDefs.h"
+#include "DependencyObject.h"
+#include "DependencyProperty.h"
+#include "Binding.h"
+
+namespace izanagi {
+	class BindingExpression;
+
+	/**
+	 */
+	class BindingOperations
+	{
+		friend class BindingExpression;
+
+	private:
+		BindingOperations();
+		~BindingOperations();
+		NO_COPIABLE(BindingOperations);
+
+	public:
+		/** データバインディングを設定.
+		 */
+		static void SetBindings(
+			IMemoryAllocator* allocator,
+			DependencyObjectBase& target,
+			DependencyProperty& property,
+			Binding* binding);
+
+		/** 指定されたプロパティに対するデータバインディングを取得.
+		 */
+		static BindingExpression* GetBindingExpression(const DependencyProperty& property);
+
+		/** 指定されたプロパティに対するデータバインディングを削除.
+		 */
+		static IZ_BOOL RemoveBindingExpression(const DependencyProperty& property);
+
+	private:
+		typedef CStdHash<IZ_UINT, BindingExpression, 5>	Dictionary;
+
+		static Dictionary s_Dictionary;
+	};
+}	// namespace izanagi
+
+#endif	// #if !defined(__IZANAGI_STD_BINDING_OPERATIONS_H__)
