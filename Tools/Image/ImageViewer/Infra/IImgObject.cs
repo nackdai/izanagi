8bb262a7a2651032b977d18ee4e95b617ceb597c
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2012-07-23 15:53:48 +0000

    Remove ImageViewerInfra.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@326 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Tools/Image/ImageViewer/Infra/IImgObject.cs
+++ Tools/Image/ImageViewer/Infra/IImgObject.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace ImageViewerInfra
+namespace ImageViewer
 {
     public interface IImgObject : IDisposable
     {
