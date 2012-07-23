6ed4b3326e87755c1b7b5166a8186ff1d10f6878
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2012-07-23 15:40:15 +0000

    - Add izTypes.h - Add auto build bat.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@324 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/izDefs.h
+++ Libs/include/izDefs.h
@@ -9,6 +9,8 @@
 #include <float.h>
 #include <limits.h>
 
+#include "izTypes.h"
+
 #ifndef WINDOWS
 	#if defined(WIN32) || defined(WIN64)
 		#define WINDOWS
@@ -26,48 +28,6 @@
 #ifndef IZ_API
 	#define IZ_API
 #endif
-
-/////////////////////////////////////////////////////////////
-// 基本
-
-#define IZ_TRUE		(1)
-#define IZ_FALSE	(0)
-
-#define IZ_NULL		(0)
-
-/////////////////////////////////////////////////////////////
-// 型
-
-typedef unsigned __int8		IZ_BYTE;
-
-typedef __int16				IZ_SHORT;
-typedef unsigned __int16	IZ_USHORT;
-
-typedef __int32				IZ_INT;
-typedef unsigned __int32	IZ_UINT;
-
-typedef __int64				IZ_LONG;
-typedef unsigned __int64	IZ_ULONG;
-
-typedef __int32				IZ_BOOL;
-
-typedef float				IZ_FLOAT;
-typedef double				IZ_DOUBLE;
-
-typedef __int8				IZ_INT8;
-typedef unsigned __int8		IZ_UINT8;
-
-typedef __int16				IZ_INT16;
-typedef unsigned __int16	IZ_UINT16;
-
-typedef __int32				IZ_INT32;
-typedef unsigned __int32	IZ_UINT32;
-
-typedef __int64				IZ_INT64;
-typedef unsigned __int64	IZ_UINT64;
-
-typedef unsigned __int16	IZ_WORD;
-typedef unsigned __int32	IZ_DWORD;
 
 #if 1
 
