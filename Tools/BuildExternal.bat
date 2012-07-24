40fce8595d9164a8f5b46a0324cf66d3088c96aa
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-07-24 15:44:11 +0000

    Modify auto build bat file.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@331 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Tools/BuildExternal.bat
+++ Tools/BuildExternal.bat
@@ -1,8 +1,16 @@
 set MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set TARGET=Rebuild
 
-%MSBUILD% External\Preproc\Preproc_VC10.sln /t:%TARGET% /p:Configuration=Release
-%MSBUILD% External\NvTriStrip\src\NvTriStrip\NvTriStrip_VC10.sln /t:%TARGET% /p:Configuration=Release
-%MSBUILD% External\xerces\projects\Win32\VC10\xerces-all\xerces-all.sln /t:XercesLib:%TARGET% /p:Configuration=Release
-%MSBUILD% External\squish\vs10\squish.sln /t:squish:%TARGET% /p:Configuration=Release
-%MSBUILD% External\collada-dom-2.4.0\dom\projects\vc10-1.4\dom.sln /t:dom-static:%TARGET% /p:Configuration=Release
+%MSBUILD% External\Preproc\Preproc_VC10.sln /t:%TARGET% /p:Configuration=Release || goto error
+%MSBUILD% External\NvTriStrip\src\NvTriStrip\NvTriStrip_VC10.sln /t:%TARGET% /p:Configuration=Release || goto error
+%MSBUILD% External\xerces\projects\Win32\VC10\xerces-all\xerces-all.sln /t:XercesLib:%TARGET% /p:Configuration=Release || goto error
+%MSBUILD% External\squish\vs10\squish.sln /t:squish:%TARGET% /p:Configuration=Release || goto error
+%MSBUILD% External\collada-dom-2.4.0\dom\projects\vc10-1.4\dom.sln /t:dom-static:%TARGET% /p:Configuration=Release || goto error
+
+@echo off
+exit /b 1
+
+:error
+@echo off
+echo "Error====="
+pause
--- Tools/BuildTools.bat
+++ Tools/BuildTools.bat
@@ -1,4 +1,12 @@
 set MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set TARGET=Rebuild
 
-%MSBUILD% projects\vs2010\Tools.sln /t:%TARGET% /p:Configuration=Release
+%MSBUILD% projects\vs2010\Tools.sln /t:%TARGET% /p:Configuration=Release || goto error
+
+@echo off
+exit /b 1
+
+:error
+@echo off
+echo "Error====="
+pause
