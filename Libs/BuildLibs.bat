f5db9e88bccc7a94137f43c412e7e7f4e6efb560
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2012-07-24 15:45:54 +0000

    Modify auto build bat file.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@332 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/BuildLibs.bat
+++ Libs/BuildLibs.bat
@@ -1,4 +1,12 @@
 set MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set TARGET=Build
 
-%MSBUILD% project\vs2010\izanagi.sln /t:%TARGET% /p:Configuration=Release_DX9
+%MSBUILD% project\vs2010\izanagi.sln /t:%TARGET% /p:Configuration=Release_DX9 || goto error
+
+@echo off
+exit /b 1
+
+:error
+@echo off
+echo "Error====="
+pause
