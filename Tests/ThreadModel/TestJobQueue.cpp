f9c44558de69101bcf5e1ef1a2dfa5a7c9db8c82
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-08-26 15:59:24 +0000

    Add ThreadModel test.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@364 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Tests/ThreadModel/TestJobQueue.cpp
@@ -0,0 +1,53 @@
+#include "izSampleKit.h"
+#include "izSystem.h"
+#include "izThreadModel.h"
+
+class CTestJob : public izanagi::threadmodel::CJob
+{
+public:
+    CTestJob(IZ_UINT id) : m_ID(id) {}
+    virtual ~CTestJob() {}
+
+public:
+    virtual void OnExecute()
+    {
+        izanagi::ThreadId id = izanagi::CThread::GetCurrentThreadId();
+
+        for (IZ_UINT i = 0; i < 100; i++)
+        {
+            IZ_PRINTF("%d: [%d] {%d}\n", id, m_ID, i);
+
+            izanagi::CThread::Sleep(10);
+        }
+    }
+
+    virtual void OnFinished()
+    {
+        IZ_PRINTF("[%d] Finish ******\n", m_ID);
+    }
+
+private:
+    IZ_UINT m_ID;
+};
+
+int TestJobQueue(izanagi::IMemoryAllocator* allocator)
+{
+    CTestJob job_0(0);
+    CTestJob job_1(1);
+    CTestJob job_2(2);
+    CTestJob job_3(3);
+
+    izanagi::threadmodel::CJobQueue jobQueue;
+    jobQueue.Init(allocator, 4);
+
+    jobQueue.Enqueue(&job_0);
+    jobQueue.Enqueue(&job_1);
+    jobQueue.Enqueue(&job_2);
+    jobQueue.Enqueue(&job_3);
+
+    jobQueue.Join();
+
+    jobQueue.Update();
+
+	return 0;
+}
---
+++ Tests/ThreadModel/main.cpp
@@ -0,0 +1,16 @@
+#include "izSampleKit.h"
+#include "izSystem.h"
+
+extern int TestJobQueue(izanagi::IMemoryAllocator* allocator);
+
+static const IZ_UINT BUF_SIZE = 1 * 1024 * 1024;
+static IZ_UINT8 BUF[BUF_SIZE];
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    izanagi::CStandardMemoryAllocator allocator(BUF_SIZE, BUF);
+
+    TestJobQueue(&allocator);
+
+	return 0;
+}
