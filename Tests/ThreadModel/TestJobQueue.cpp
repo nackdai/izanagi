f3b7acd0934af5663d26ed5643879535cad56109
nakdai <newvf91.brjtn579.dn@gmail.com>
2012-10-22 13:13:47 +0000

    Modify TestJobQueue.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@373 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Tests/ThreadModel/TestJobQueue.cpp
+++ Tests/ThreadModel/TestJobQueue.cpp
@@ -30,7 +30,7 @@ private:
     IZ_UINT m_ID;
 };
 
-int TestJobQueue(izanagi::IMemoryAllocator* allocator)
+void Test_0(izanagi::IMemoryAllocator* allocator)
 {
     CTestJob job_0(0);
     CTestJob job_1(1);
@@ -40,17 +40,71 @@ int TestJobQueue(izanagi::IMemoryAllocator* allocator)
     izanagi::threadmodel::CJobQueue jobQueue;
     jobQueue.Init(allocator, 4);
 
+    jobQueue.Start();
+
     jobQueue.Enqueue(&job_0);
     jobQueue.Enqueue(&job_1);
     jobQueue.Enqueue(&job_2);
     jobQueue.Enqueue(&job_3);
 
-    //jobQueue.Join();
-    jobQueue.WaitForFinishJobQueue();
+    jobQueue.Join();
 
     jobQueue.Update();
+}
+
+void Test_1(izanagi::IMemoryAllocator* allocator)
+{
+    CTestJob job_0(0);
+    CTestJob job_1(1);
+    CTestJob job_2(2);
+    CTestJob job_3(3);
+
+    izanagi::threadmodel::CJobQueue jobQueue;
+    jobQueue.Init(allocator, 4);
+
+    jobQueue.Start();
+
+    jobQueue.Enqueue(&job_0);
+    jobQueue.Enqueue(&job_1);
+    jobQueue.Enqueue(&job_2);
+    jobQueue.Enqueue(&job_3);
 
     jobQueue.Join();
 
-	return 0;
+    jobQueue.Update();
+}
+
+void Test_2(izanagi::IMemoryAllocator* allocator)
+{
+    CTestJob job_0(0);
+    CTestJob job_1(1);
+    CTestJob job_2(2);
+    CTestJob job_3(3);
+
+    izanagi::threadmodel::CJobQueue jobQueue;
+    jobQueue.Init(allocator, 4);
+
+    jobQueue.Start();
+
+    jobQueue.Enqueue(&job_0);
+    jobQueue.Enqueue(&job_1);
+    jobQueue.Enqueue(&job_2);
+    jobQueue.Enqueue(&job_3);
+
+    job_3.Cancel();
+
+    jobQueue.Join();
+
+    jobQueue.Update();
+}
+
+int TestJobQueue(izanagi::IMemoryAllocator* allocator)
+{
+    //Test_0(allocator);
+
+    //Test_1(allocator);
+
+    Test_2(allocator);
+
+    return 0;
 }
