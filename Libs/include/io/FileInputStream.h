076e2d08310ac5242a090941c1f3fb4c2a4f334a
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-01-10 14:08:17 +0000

    - Refactoring code. - It is enabled to specify file open mode.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@465 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/io/FileInputStream.h
+++ Libs/include/io/FileInputStream.h
@@ -3,200 +3,159 @@
 
 #include "InputStream.h"
 
-namespace izanagi {
+namespace izanagi
+{
     /**
     * 超簡易ファイル入力
     */
-    class CFileInputStream : public IInputStream {
+    class CFileInputStream : public IInputStream
+    {
     public:
-        inline CFileInputStream();
-        inline ~CFileInputStream();
+        CFileInputStream()
+        {
+            m_File = IZ_NULL;
+            m_Size = 0;
+            m_Pos = 0;
+        }
+        ~CFileInputStream()
+        {
+            Close();
+        }
 
         NO_COPIABLE(CFileInputStream);
 
     public:
         // ファイルオープン
-        inline IZ_BOOL Open(IZ_PCSTR pszFile);
+        IZ_BOOL Open(IZ_PCSTR path)
+        {
+            IZ_BOOL ret = IZ_FALSE;
+
+            if (m_File != IZ_NULL) {
+                IZ_ASSERT(IZ_FALSE);
+                Close();
+            }
+
+            fopen_s(&m_File, path, "rb");
+            ret = (m_File != IZ_NULL);
+
+            if (ret) {
+                // ファイルサイズ取得
+                fseek(m_File, 0, SEEK_END);
+                m_Size = ftell(m_File);
+                fseek(m_File, 0, SEEK_SET);
+            }
+
+            return ret;
+        }
 
         // ファイルクローズ
-        inline void Close();
+        void Close()
+        {
+            if (m_File != IZ_NULL) {
+                fclose(m_File);
+            }
+
+            m_File = IZ_NULL;
+            m_Size = 0;
+            m_Pos = 0;
+        }
 
         // 読み込み
-        inline IZ_UINT Read(void* buf, IZ_INT offset, size_t size);
+        IZ_UINT Read(void* buf, IZ_INT offset, size_t size)
+        {
+            IZ_ASSERT(m_File != IZ_NULL);
+
+            if (offset > 0) {
+                VRETURN(Seek(offset, E_IO_STREAM_SEEK_POS_CUR));
+            }
+            IZ_UINT ret = (IZ_UINT)fread(buf, 1, size, m_File);
+
+            if (ret == size) {
+                // TODO
+                // ファイル位置更新
+                m_Pos = m_Pos + (IZ_INT)size;
+            }
+            else {
+                m_Pos = m_Size;
+            }
+
+            return ret;
+        }
 
         // サイズ取得
-        inline IZ_LONG GetSize();
+        IZ_LONG GetSize()
+        {
+            return m_Size;
+        }
 
         // 現在位置取得
-        inline IZ_LONG GetCurPos();
+        IZ_LONG GetCurPos()
+        {
+            return m_Pos;
+        }
 
         // シーク
-        inline IZ_BOOL Seek(IZ_INT nOffset, E_IO_STREAM_SEEK_POS nSeekPos);
+        IZ_BOOL Seek(IZ_INT nOffset, E_IO_STREAM_SEEK_POS nSeekPos)
+        {
+            IZ_ASSERT(m_File != IZ_NULL);
+
+            IZ_UINT nPos = SEEK_SET;
+            IZ_LONG offset = static_cast<IZ_LONG>(nOffset);
+
+            switch (nSeekPos) {
+            case E_IO_STREAM_SEEK_POS_START:
+                nPos = SEEK_SET;
+                offset = CLAMP(offset, 0, m_Size);
+                break;
+            case E_IO_STREAM_SEEK_POS_CUR:
+                nPos = SEEK_CUR;
+                offset = CLAMP(offset, -m_Pos, m_Size - m_Pos);
+                break;
+            case E_IO_STREAM_SEEK_POS_END:
+                nPos = SEEK_END;
+                offset = CLAMP(offset, -m_Size, 0);
+                break;
+            }
+
+            // TODO
+            IZ_ASSERT(IS_IN_BOUND(offset, IZ_INT32_MIN, IZ_INT32_MAX));
+
+            IZ_BOOL ret = IZ_TRUE;
+            if (nOffset != 0) {
+                // シーク
+                ret = (fseek(m_File, static_cast<IZ_INT>(offset), nPos) == 0);
+
+                if (ret) {
+                    // 現在位置更新
+                    switch (nSeekPos) {
+                    case E_IO_STREAM_SEEK_POS_START:
+                        m_Pos = offset;
+                        break;
+                    case E_IO_STREAM_SEEK_POS_CUR:
+                        m_Pos += offset;
+                        break;
+                    case E_IO_STREAM_SEEK_POS_END:
+                        m_Pos = m_Size + offset;
+                        break;
+                    }
+                }
+            }
+            IZ_ASSERT(ret);
+
+            return ret;
+        }
 
         // 終了処理
-        inline void Finalize();
-
-    protected:
-        FILE* m_pFile;
-        IZ_LONG m_nSize;
-        IZ_LONG m_nPos;
-    };
-
-    // inline **************************************
-
-    // コンストラクタ
-    CFileInputStream::CFileInputStream()
-    {
-        m_pFile = IZ_NULL;
-        m_nSize = 0;
-        m_nPos = 0;
-    }
-
-    // デストラクタ
-    CFileInputStream::~CFileInputStream()
-    {
-        Close();
-    }
-
-    /**
-    * ファイルオープン
-    */
-    IZ_BOOL CFileInputStream::Open(IZ_PCSTR pszFile)
-    {
-        IZ_BOOL ret = IZ_FALSE;
-
-        if (m_pFile != IZ_NULL) {
-            IZ_ASSERT(IZ_FALSE);
+        void Finalize()
+        {
             Close();
         }
 
-        fopen_s(&m_pFile, pszFile, "rb");
-        ret = (m_pFile != IZ_NULL);
-
-        if (ret) {
-            // ファイルサイズ取得
-            fseek(m_pFile, 0, SEEK_END);
-            m_nSize = ftell(m_pFile);
-            fseek(m_pFile, 0, SEEK_SET);
-        }
-
-        return ret;
-    }
-
-    /**
-    * ファイルクローズ
-    */
-    void CFileInputStream::Close()
-    {
-        if (m_pFile != IZ_NULL) {
-            fclose(m_pFile);
-        }
-
-        m_pFile = IZ_NULL;
-        m_nSize = 0;
-        m_nPos = 0;
-    }
-
-    /**
-    * 読み込み
-    */
-    IZ_UINT CFileInputStream::Read(void* buf, IZ_INT offset, size_t size)
-    {
-        IZ_ASSERT(m_pFile != IZ_NULL);
-
-        if (offset > 0) {
-            VRETURN(Seek(offset, E_IO_STREAM_SEEK_POS_CUR));
-        }
-        IZ_UINT ret = (IZ_UINT)fread(buf, 1, size, m_pFile);
-
-        if (ret == size) {
-            // TODO
-            // ファイル位置更新
-            m_nPos = m_nPos + (IZ_INT)size;
-        }
-        else {
-            m_nPos = m_nSize;
-        }
-
-        return ret;
-    }
-
-    /**
-    * サイズ取得
-    */
-    IZ_LONG CFileInputStream::GetSize()
-    {
-        return m_nSize;
-    }
-
-    /**
-    * 現在位置取得
-    */
-    IZ_LONG CFileInputStream::GetCurPos()
-    {
-        return m_nPos;
-    }
-
-    /**
-    * シーク
-    */
-    IZ_BOOL CFileInputStream::Seek(IZ_INT nOffset, E_IO_STREAM_SEEK_POS nSeekPos)
-    {
-        IZ_ASSERT(m_pFile != IZ_NULL);
-
-        IZ_UINT nPos = SEEK_SET;
-        IZ_LONG offset = static_cast<IZ_LONG>(nOffset);
-
-        switch (nSeekPos) {
-        case E_IO_STREAM_SEEK_POS_START:
-            nPos = SEEK_SET;
-            offset = CLAMP(offset, 0, m_nSize);
-            break;
-        case E_IO_STREAM_SEEK_POS_CUR:
-            nPos = SEEK_CUR;
-            offset = CLAMP(offset, -m_nPos, m_nSize - m_nPos);
-            break;
-        case E_IO_STREAM_SEEK_POS_END:
-            nPos = SEEK_END;
-            offset = CLAMP(offset, -m_nSize, 0);
-            break;
-        }
-
-        // TODO
-        IZ_ASSERT(IS_IN_BOUND(offset, IZ_INT32_MIN, IZ_INT32_MAX));
-
-        IZ_BOOL ret = IZ_TRUE;
-        if (nOffset != 0) {
-            // シーク
-            ret = (fseek(m_pFile, static_cast<IZ_INT>(offset), nPos) == 0);
-
-            if (ret) {
-                // 現在位置更新
-                switch (nSeekPos) {
-                case E_IO_STREAM_SEEK_POS_START:
-                    m_nPos = offset;
-                    break;
-                case E_IO_STREAM_SEEK_POS_CUR:
-                    m_nPos += offset;
-                    break;
-                case E_IO_STREAM_SEEK_POS_END:
-                    m_nPos = m_nSize + offset;
-                    break;
-                }
-            }
-        }
-        IZ_ASSERT(ret);
-
-        return ret;
-    }
-
-    /**
-    * 終了処理
-    */
-    void CFileInputStream::Finalize()
-    {
-        Close();
-    }
+    protected:
+        FILE* m_File;
+        IZ_LONG m_Size;
+        IZ_LONG m_Pos;
+    };
 }   // namespace izanagi
 
 #endif  // #if !defined(__IZANAGI_IO_FILE_INPUT_STREAM_H__)
--- Libs/include/io/FileOutputStream.h
+++ Libs/include/io/FileOutputStream.h
@@ -3,180 +3,160 @@
 
 #include "OutputStream.h"
 
-namespace izanagi {
+namespace izanagi
+{
     /**
-    */
-    class CFileOutputStream : public IOutputStream {
+     */
+    class CFileOutputStream : public IOutputStream
+    {
     public:
-        inline CFileOutputStream();
-        inline ~CFileOutputStream();
+        CFileOutputStream()
+        {
+            m_File = IZ_NULL;
+            m_Pos = 0;
+        }
+        ~CFileOutputStream()
+        {
+            Finalize();
+        }
 
         NO_COPIABLE(CFileOutputStream);
 
     public:
-        inline IZ_BOOL Open(IZ_PCSTR pszPath);
+        IZ_BOOL Open(IZ_PCSTR path)
+        {
+            return Open(path, E_IO_FILE_MODE_BIN);
+        }
 
-        inline void Finalize();
+        IZ_BOOL Open(IZ_PCSTR path, E_IO_FILE_MODE mode)
+        {
+            IZ_BOOL ret = IZ_FALSE;
+
+            if (m_File != IZ_NULL) {
+                IZ_ASSERT(IZ_FALSE);
+                Finalize();
+            }
+
+            fopen_s(
+                &m_File, 
+                path,
+                mode == E_IO_FILE_MODE_TEXT ? "wt" : "wb");
+            ret = (m_File != IZ_NULL);
+
+            return ret;
+        }
+
+        void Finalize()
+        {
+            if (m_File != IZ_NULL) {
+                fclose(m_File);
+            }
+
+            m_File = IZ_NULL;
+            m_Pos = 0;
+        }
 
         // 出力
-        inline IZ_UINT Write(const void* buf, IZ_UINT offset, size_t size);
+        IZ_UINT Write(const void* buf, IZ_UINT offset, size_t size)
+        {
+            IZ_ASSERT(m_File != IZ_NULL);
+            IZ_ASSERT(size > 0);
+
+            if (offset > 0) {
+                Seek(offset, E_IO_STREAM_SEEK_POS_CUR);
+            }
+
+            IZ_UINT ret = (IZ_UINT)fwrite(buf, 1, size, m_File);
+            IZ_ASSERT(ret == size);
+
+            if (ret == size) {
+                m_Pos += offset + size;
+            }
+
+            return ret;
+        }
+
+        IZ_BOOL Write(IZ_PCSTR text)
+        {
+            IZ_ASSERT(m_File != IZ_NULL);
+
+            int result = ::fprintf_s(m_File, "%s", text);
+            IZ_ASSERT(result >= 0);
+
+            return (result >= 0);
+        }
 
         // サイズ取得
-        inline IZ_UINT GetSize();
+        IZ_UINT GetSize()
+        {
+            IZ_ASSERT(m_File != IZ_NULL);
+
+            IZ_UINT ret = ftell(m_File);
+            return ret;
+        }
 
         // 現在位置取得
-        inline IZ_UINT GetCurPos();
+        IZ_UINT GetCurPos()
+        {
+            return m_Pos;
+        }
 
         // シーク
-        inline IZ_BOOL Seek(IZ_INT nOffset, E_IO_STREAM_SEEK_POS nSeekPos);
+        IZ_BOOL Seek(IZ_INT offset, E_IO_STREAM_SEEK_POS seekPos)
+        {
+            IZ_ASSERT(m_File != IZ_NULL);
 
-        inline IZ_BOOL IsValid();
+            IZ_UINT nPos = SEEK_SET;
+
+            switch (seekPos) {
+            case E_IO_STREAM_SEEK_POS_START:
+                nPos = SEEK_SET;
+                break;
+            case E_IO_STREAM_SEEK_POS_CUR:
+                nPos = SEEK_CUR;
+                break;
+            case E_IO_STREAM_SEEK_POS_END:
+                // 出力時にはファイル終端は存在しないので・・・
+                VRETURN(IZ_FALSE);
+                break;
+            }
+
+            IZ_BOOL ret = IZ_TRUE;
+            {
+                // シーク
+                ret = (fseek(m_File, offset, nPos) == 0);
+
+                if (ret) {
+                    // 現在位置更新
+                    switch (seekPos) {
+                    case E_IO_STREAM_SEEK_POS_START:
+                        m_Pos = offset;
+                        break;
+                    case E_IO_STREAM_SEEK_POS_CUR:
+                        m_Pos += offset;
+                        break;
+                    case E_IO_STREAM_SEEK_POS_END:
+                        VRETURN(IZ_FALSE);
+                        break;
+                    }
+                }
+            }
+            IZ_ASSERT(ret);
+
+            return ret;
+        }
+
+        IZ_BOOL IsValid()
+        {
+            return (m_File != NULL);
+        }
 
         IZ_BOOL IsEnableOutput() { return IsValid(); }
 
     private:
-        FILE* m_pFile;
-        IZ_UINT m_nPos;
+        FILE* m_File;
+        IZ_UINT m_Pos;
     };
-
-    // inline *******************************
-
-    // コンストラクタ
-    CFileOutputStream::CFileOutputStream()
-    {
-        m_pFile = IZ_NULL;
-        m_nPos = 0;
-    }
-
-    // デストラクタ
-    CFileOutputStream::~CFileOutputStream()
-    {
-        Finalize();
-    }
-
-    /**
-    * ファイルオープン
-    */
-    IZ_BOOL CFileOutputStream::Open(IZ_PCSTR pszFile)
-    {
-        IZ_BOOL ret = IZ_FALSE;
-
-        if (m_pFile != IZ_NULL) {
-            IZ_ASSERT(IZ_FALSE);
-            Finalize();
-        }
-
-        fopen_s(&m_pFile, pszFile, "wb");
-        ret = (m_pFile != IZ_NULL);
-
-        return ret;
-    }
-
-    /**
-    * ファイルクローズ
-    */
-    void CFileOutputStream::Finalize()
-    {
-        if (m_pFile != IZ_NULL) {
-            fclose(m_pFile);
-        }
-
-        m_pFile = IZ_NULL;
-        m_nPos = 0;
-    }
-
-    /**
-    * 出力
-    */
-    IZ_UINT CFileOutputStream::Write(const void* buf, IZ_UINT offset, size_t size)
-    {
-        IZ_ASSERT(m_pFile != IZ_NULL);
-        IZ_ASSERT(size > 0);
-
-        if (offset > 0) {
-            Seek(offset, E_IO_STREAM_SEEK_POS_CUR);
-        }
-
-        IZ_UINT ret = (IZ_UINT)fwrite(buf, 1, size, m_pFile);
-        IZ_ASSERT(ret == size);
-
-        if (ret == size) {
-            m_nPos += offset + size;
-        }
-
-        return ret;
-    }
-
-    /**
-    * サイズ取得
-    */
-    IZ_UINT CFileOutputStream::GetSize()
-    {
-        IZ_ASSERT(m_pFile != IZ_NULL);
-
-        IZ_UINT ret = ftell(m_pFile);
-        return ret;
-    }
-
-    /**
-    * 現在位置取得
-    */
-    IZ_UINT CFileOutputStream::GetCurPos()
-    {
-        return m_nPos;
-    }
-
-    /**
-    * シーク
-    */
-    IZ_BOOL CFileOutputStream::Seek(IZ_INT nOffset, E_IO_STREAM_SEEK_POS nSeekPos)
-    {
-        IZ_ASSERT(m_pFile != IZ_NULL);
-
-        IZ_UINT nPos = SEEK_SET;
-
-        switch (nSeekPos) {
-        case E_IO_STREAM_SEEK_POS_START:
-            nPos = SEEK_SET;
-            break;
-        case E_IO_STREAM_SEEK_POS_CUR:
-            nPos = SEEK_CUR;
-            break;
-        case E_IO_STREAM_SEEK_POS_END:
-            // 出力時にはファイル終端は存在しないので・・・
-            VRETURN(IZ_FALSE);
-            break;
-        }
-
-        IZ_BOOL ret = IZ_TRUE;
-        {
-            // シーク
-            ret = (fseek(m_pFile, nOffset, nPos) == 0);
-
-            if (ret) {
-                // 現在位置更新
-                switch (nSeekPos) {
-                case E_IO_STREAM_SEEK_POS_START:
-                    m_nPos = nOffset;
-                    break;
-                case E_IO_STREAM_SEEK_POS_CUR:
-                    m_nPos += nOffset;
-                    break;
-                case E_IO_STREAM_SEEK_POS_END:
-                    VRETURN(IZ_FALSE);
-                    break;
-                }
-            }
-        }
-        IZ_ASSERT(ret);
-
-        return ret;
-    }
-
-    IZ_BOOL CFileOutputStream::IsValid()
-    {
-        return (m_pFile != NULL);
-    }
 }   // namespace izanagi
 
 #endif  // #if !defined(__IZANAGI_IO_FILE_OUTPUT_STREAM_H__)
