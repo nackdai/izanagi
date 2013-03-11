c8e37a8995f7171b1469175273a216ca81717b0f
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2013-03-11 13:13:50 +0000

    - Use baseline for text rendering. - Modify sample strings.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@522 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Samples/FontBitmap/FontUTF8.h
+++ Samples/FontBitmap/FontUTF8.h
@@ -1,6 +1,6 @@
 #if !defined(__FONT_UTF8_H__)
 #define __FONT_UTF8_H__
 
-const char* fontUTF8 = "UTF8 \x82\x81\xe3\x84\x81\xe3\x86\x81\xe3\x88\x81\xe3\x8a\x81\xe3";
+const char* fontUTF8 = "UTF8 \xe3\x81\x82\xe3\x81\x84\xe3\x81\x86\xe3\x81\x88\xe3\x81\x8a";
 
 #endif    // #define __FONT_UTF8_H__
