3e4a7659151ebd6e004e3ea33abd8467fe9b3889
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2013-03-16 15:40:33 +0000

    Add FontFreeType sample.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@538 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Samples/FontFreeType/FontUTF8.h
@@ -0,0 +1,6 @@
+#if !defined(__FONT_UTF8_H__)
+#define __FONT_UTF8_H__
+
+const char* fontUTF8 = "UTF8 \xe3\x81\x82\xe3\x81\x84\xe3\x81\x86\xe3\x81\x88\xe3\x81\x8a";
+
+#endif    // #define __FONT_UTF8_H__
