40ed32d402d4c0ba331344d8b12967e20e7446be
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-05-04 08:32:25 +0000

    Add perl script for bridge between git and svn.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@626 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ GitSvnMerge.pl
@@ -0,0 +1,41 @@
+#!/bin/perl
+
+my $tag = "SVN_MERGED_TAG";
+
+# fetch svn
+DoCmd("git svn fetch");
+
+# pull origin/master
+DoCmd("git checkout master");
+DoCmd("git pull origin master");
+
+# Get commit hash
+print "git log --pretty=oneline --reverse master...$tag\n";
+my @logs = `git log --pretty=oneline --reverse master...$tag`;
+
+# merge
+DoCmd("git checkout git-svn");
+
+foreach my $log (@logs)
+{
+	my $hash = (split(/\s/, $log))[0];
+
+	DoCmd("git cherry-pick $hash");
+}
+
+# svn commit
+DoCmd("git svn dcommit");
+
+# É^ÉO
+DoCmd("git checkout master");
+DoCmd("git tag $tag");
+DoCmd("git push -f origin $tag");
+
+#=========================================
+
+sub DoCmd
+{
+	my $cmd = shift;
+	print $cmd . "\n";
+	system($cmd);
+}
