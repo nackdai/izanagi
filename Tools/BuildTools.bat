35289425858b71d80ce69f4dba0350a128eb0148
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2013-08-18 16:38:50 +0000

    Build Math.lib before building Tools.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@730 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Tools/BuildTools.bat
+++ Tools/BuildTools.bat
@@ -2,6 +2,7 @@ set MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 set TARGET=Build
 set CONFIG=%1
 
+%MSBUILD% ..\Libs\project\vs2010\Math.vcxproj /t:%TARGET% /p:Configuration=%CONFIG%_DX9 || goto error
 %MSBUILD% projects\vs2010\Tools.sln /t:%TARGET% /p:Configuration=%CONFIG% || goto error
 
 @echo off
