94ebd88bc83a037be09fc29fe5c805f0c0ad8a91
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-09-15 15:55:15 +0000

    - Re-architecture ResourceManager. - Remove ARC code from project temporarily.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@796 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/resource/Resource.h
+++ Libs/include/resource/Resource.h
@@ -1,73 +1,45 @@
 #if !defined(__IZANAGI_RESOURCE_RESOURCE_H__)
 #define __IZANAGI_RESOURCE_RESOURCE_H__
 
-#include "std/StdPlacementNew.h"
-#include "std/StdObject.h"
-#include "std/allocator/MemoryAllocator.h"
-#include "ResourceManager.h"
+#include "izStd.h"
+#include "resource/ResourceManager.h"
 
-namespace izanagi {
-    /**
-    */
+namespace izanagi
+{
+namespace resource
+{
     class CResource : public CPlacementNew {
         friend class CResourceManager;
 
-    protected:
-        CResource();
-        ~CResource() { Clear(); }
+        static CResource* CreateResource(
+            IMemoryAllocator* allocator,
+            IZ_UINT type,
+            const CKey& key);
 
-        NO_COPIABLE(CResource);
+    private:
+        CResource(IZ_UINT type, const CKey& key);
+        virtual ~CResource();
 
-    protected:
-        IZ_BOOL Init(
-            CResourceManager* pRscMgr,
-            CObject* pData,
-            const CKey& cKey,
-            const CRscType& cType);
+    private:
+        void SetObject(CObject* object);
+        CObject* GetObject();
 
+        void Init(IZ_UINT type, const CKey& key);
         void Clear();
 
-        void ReleaseResource();
+        IZ_UINT GetType() { return m_Type; }
 
-        // Return whether this has data.
-        IZ_BOOL IsValid() const { return (m_pData != IZ_NULL); }
+    private:
+        CResourceManager::RscHash::Item* GetHashItem() { return &m_HashItem; }
 
-        void SetAllocator(IMemoryAllocator* pAllocator) { m_Allocator = pAllocator; }
-        IMemoryAllocator* GetAllocator() { return m_Allocator; }
+    private:
+        CObject* m_Object;
 
-        CResourceManager::CRscHashItem* GetHashItem() { return &m_HashItem; }
-        CResourceManager::CFreeRscListItem* GetFreeListItem() { return &m_FreeListItem; }
+        IZ_UINT m_Type;
 
-    public:
-        // Return resource type.
-        const CRscType& GetType() const { return m_Type; }
-        IZ_PCSTR GetTypeAsString() const { return m_Type.GetString(); }
-
-        // Return resource raw data.
-        CObject* GetData() { return m_pData; }
-
-        // Return resource raw data.
-        template <typename _T>
-        _T* GetData() { return reinterpret_cast<_T*>(m_pData); }
-
-        // Return resource id.
-        const CKey& GetKey() const { m_HashItem.GetKey(); }
-
-    protected:
-        enum {
-            STR_SIZE = 7,
-        };
-
-        IMemoryAllocator* m_Allocator;
-
-        CRscType m_Type;
-        CObject* m_pData;
-
-        CResourceManager::CRscHashItem m_HashItem;
-        CResourceManager::CFreeRscListItem m_FreeListItem;
-
-        CResourceManager* m_pRscMgr;
+        CResourceManager::RscHash::Item m_HashItem;
     };
+}   // namespace resource
 }   // namespace izanagi
 
 #endif  // #if !defined(__IZANAGI_RESOURCE_RESOURCE_H__)
--- Libs/source/resource/Resource.cpp
+++ Libs/source/resource/Resource.cpp
@@ -1,46 +1,61 @@
 #include "resource/Resource.h"
 
-using namespace izanagi;
-
-CResource::CResource()
+namespace izanagi
 {
-    m_Allocator = IZ_NULL;
-
-    m_pData = IZ_NULL;
-    m_pRscMgr = IZ_NULL;
-
-    m_FreeListItem.Init(this);
-}
-
-IZ_BOOL CResource::Init(
-    CResourceManager* pRscMgr,
-    CObject* pData,
-    const CKey& cKey,
-    const CRscType& cType)
+namespace resource
 {
-    IZ_ASSERT(pData != IZ_NULL);
+    CResource* CResource::CreateResource(
+        IMemoryAllocator* allocator,
+        IZ_UINT type,
+        const CKey& key)
+    {
+        void* buf = ALLOC(allocator, sizeof(CResource));
+        VRETURN_NULL(buf != IZ_NULL);
 
-    m_Type = cType;
-    SAFE_REPLACE(m_pData, pData);
+        CResource* instance = new(buf) CResource(type, key);
+        return instance;
+    }
 
-    m_HashItem.Init(cKey, this);
-    m_FreeListItem.Init(this);
+    CResource::CResource(IZ_UINT type, const CKey& key)
+    {
+        m_Object = IZ_NULL;
 
-    m_pRscMgr = pRscMgr;
+        m_Type = type;
 
-    return IZ_TRUE;
-}
+        m_HashItem.Init(key, this);
+    }
 
-void CResource::Clear()
-{
-    m_Type.Clear();
-    SAFE_RELEASE(m_pData);
-    m_HashItem.Leave();
-    m_FreeListItem.Leave();
-}
+    CResource::~CResource()
+    {
+        SAFE_RELEASE(m_Object);
+    }
 
-void CResource::ReleaseResource()
-{
-    IZ_ASSERT(m_pRscMgr != IZ_NULL);
-    m_pRscMgr->RemoveResource(GetHashItem(), IZ_TRUE);
-}
+    void CResource::SetObject(CObject* object)
+    {
+        SAFE_REPLACE(m_Object, object);
+    }
+
+    CObject* CResource::GetObject()
+    {
+        return m_Object;
+    }
+
+    void CResource::Init(IZ_UINT type, const CKey& key)
+    {
+        m_HashItem.Leave();
+
+        m_Type = type;
+        m_HashItem.Init(key, this);
+    }
+
+    void CResource::Clear()
+    {
+        SAFE_RELEASE(m_Object);
+
+        m_Type = 0;
+
+        m_HashItem.Leave();
+        m_HashItem.Init(CKey::ZERO, this);
+    }
+}   // namespace resource
+}   // namespace izanagi
