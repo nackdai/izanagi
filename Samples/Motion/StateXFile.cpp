80ac3efe0ad068a2616c861999bbb42e326bd326
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2013-10-19 17:48:40 +0000

    Add states. - It is enabled to change rendering collada and xfile.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@842 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Samples/Motion/StateXFile.cpp
@@ -0,0 +1,46 @@
+#include "StateXFile.h"
+#include "izGraph.h"
+#include "izIo.h"
+#include "StateManager.h"
+
+CStateXFile::CStateXFile(izanagi::sample::CSampleApp* app)
+: CStateBase(app)
+{
+}
+
+CStateXFile::~CStateXFile()
+{
+    Destroy();
+}
+
+// 開始
+IZ_BOOL CStateXFile::Enter(
+    izanagi::IMemoryAllocator* allocator,
+    void* val)
+{
+    izanagi::graph::CGraphicsDevice* device = reinterpret_cast<izanagi::graph::CGraphicsDevice*>(val);
+
+    IZ_BOOL result = InitObject(
+        allocator,
+        device,
+        300.0f,
+        "data/ModelImage.img",
+        "data/tiny.msh",
+        "data/tiny.skl",
+        "data/tiny.anm",
+        "data/SkinShader.shd");
+
+    return result;
+}
+
+// ステートから抜ける（終了）.
+IZ_BOOL CStateXFile::Leave()
+{
+    ReleaseObject();
+    return IZ_TRUE;
+}
+
+IZ_FLOAT CStateXFile::GetElapesedTime()
+{
+    return 33.333f;
+}
