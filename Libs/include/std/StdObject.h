bfc3eef6c86457bf767067bed5de8769a9bd76ce
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-11-02 08:31:33 +0000

    Add ObjectReference.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@851 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/include/std/StdObject.h
+++ Libs/include/std/StdObject.h
@@ -4,11 +4,56 @@
 #include "izDefs.h"
 
 namespace izanagi {
-    class CObject {
+    class ObjectReference;
+
+    /**
+     */
+    class CObject
+    {
+        friend class ObjectReference;
+
+        struct Callback {
+            void operator()()
+            {
+                (reference->*func)();
+            }
+
+            void Remove()
+            {
+                if (prev) {
+                    prev->next = next;
+                }
+                if (next) {
+                    next->prev = prev;
+                }
+
+                prev = next = IZ_NULL;
+            }
+
+            typedef void (ObjectReference::* Func)();
+
+            ObjectReference* reference;
+            Func func;
+
+            Callback* prev;
+            Callback* next;
+
+            Callback()
+            {
+                prev = next = IZ_NULL;
+            }
+
+            ~Callback()
+            {
+                Remove();
+            }
+        };
+
     protected:
         CObject()
         {
             m_nRefCnt = 0;
+            m_Callback = IZ_NULL;
         }
         virtual ~CObject() {}
 
@@ -28,18 +73,40 @@ namespace izanagi {
                 m_nRefCnt--;
 
                 if (m_nRefCnt == 0) {
+                    if (m_Callback) {
+                        Callback* callback = m_Callback;
+                        while (callback) {
+                            (*callback)();
+                            Callback* next = callback->next;
+                            callback->Remove();
+                            callback = next;
+                        }
+                    }
+
                     InternalRelease();
                 }
             }
         }
 
-        // 参照カウンタ取得
-        IZ_INT GetRefCnt() const
+    private:
+        void AddCallback(Callback* callback)
         {
-            return m_nRefCnt;
+            if (m_Callback) {
+                m_Callback->prev = callback;
+                callback->next = m_Callback;
+            }
+            m_Callback = callback;
         }
 
-    private:
+        void RemoveCallback(Callback* callback)
+        {
+            callback->Remove();
+
+            if (m_Callback == callback) {
+                m_Callback = m_Callback->next;
+            }
+        }
+
         void* operator new(size_t size)
         {
             // Don't uset..
@@ -67,6 +134,83 @@ namespace izanagi {
     private:
         // 参照カウンタ
         IZ_INT m_nRefCnt;
+
+        Callback* m_Callback;
+    };
+
+    /**
+     */
+    class ObjectReference
+    {
+    public:
+        ObjectReference()
+        {
+            Init();
+        }
+        
+        ObjectReference(CObject* obj)
+        {
+            Init();
+            Set(obj);
+        }
+
+        ObjectReference(const ObjectReference& rhs)
+        {
+            Init();
+            Set(rhs.m_Obj);
+        }
+
+        const ObjectReference& operator=(const ObjectReference& rhs)
+        {
+            Init();
+            Set(rhs.m_Obj);
+        }
+
+        ~ObjectReference()
+        {
+        }
+
+        void Init()
+        {
+            m_Callback.reference = this;
+            m_Callback.func = &ObjectReference::ListenObjectReleased;
+            m_Obj = IZ_NULL;
+        }
+
+    public:
+        IZ_BOOL IsAlive() const
+        {
+            return (m_Obj != IZ_NULL);
+        }
+
+        CObject* GetTarget()
+        {
+            return m_Obj;
+        }
+
+        void Set(CObject* obj)
+        {
+            if (m_Obj) {
+                m_Obj->RemoveCallback(&m_Callback);
+            }
+
+            m_Obj = obj;
+
+            if (m_Obj) {
+                m_Obj->AddCallback(&m_Callback);
+            }
+        }
+
+    private:
+        void ListenObjectReleased()
+        {
+            m_Obj = IZ_NULL;
+        }
+
+    private:
+        CObject* m_Obj;
+
+        CObject::Callback m_Callback;
     };
 }   // namespace izanagi
 
