8a613555fd558a1d234eef18cf326bd8ab1fd355
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-11-02 16:39:24 +0000

    Add WaterDrop sample.(This is temporary)
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@862 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Samples/WaterDrop/WaterDrop.cpp
@@ -0,0 +1,114 @@
+#include "izSampleKit.h"
+
+class WaterDropApp : public izanagi::sample::CSampleApp {
+public:
+    WaterDropApp();
+    virtual ~WaterDropApp();
+
+protected:
+    // 初期化.
+    virtual IZ_BOOL InitInternal(
+        izanagi::IMemoryAllocator* allocator,
+        izanagi::graph::CGraphicsDevice* device,
+        izanagi::sample::CSampleCamera& camera);
+
+    // 解放.
+    virtual void ReleaseInternal();
+
+    // 更新.
+    virtual void UpdateInternal(izanagi::graph::CGraphicsDevice* device);
+
+    // 描画.
+    virtual void RenderInternal(izanagi::graph::CGraphicsDevice* device);
+
+private:
+    izanagi::CImage* m_Img;
+};
+
+WaterDropApp::WaterDropApp()
+{
+    m_Img = IZ_NULL;
+}
+
+WaterDropApp::~WaterDropApp()
+{
+}
+
+// 初期化.
+IZ_BOOL WaterDropApp::InitInternal(
+    izanagi::IMemoryAllocator* allocator,
+    izanagi::graph::CGraphicsDevice* device,
+    izanagi::sample::CSampleCamera& camera)
+{
+    izanagi::CFileInputStream in;
+    VRETURN(in.Open("data/earth.img"));
+
+    m_Img = izanagi::CImage::CreateImage(
+                allocator,
+                device,
+                &in);
+
+    return (m_Img != IZ_NULL);
+}
+
+// 解放.
+void WaterDropApp::ReleaseInternal()
+{
+    SAFE_RELEASE(m_Img);
+}
+
+// 更新.
+void WaterDropApp::UpdateInternal(izanagi::graph::CGraphicsDevice* device)
+{
+    // Nothing is done...
+}
+
+// 描画.
+void WaterDropApp::RenderInternal(izanagi::graph::CGraphicsDevice* device)
+{
+    if (device->Begin2D()) {
+        // スプライト
+        device->SetTexture(0, m_Img->GetTexture(0));
+        device->Set2DRenderOp(izanagi::graph::E_GRAPH_2D_RENDER_OP_MODULATE);
+        device->Draw2DSprite(
+            izanagi::CFloatRect(0.0f, 0.0f, 1.0f, 1.0f),
+            izanagi::CIntRect(300, 100, 556, 228));
+
+        // 塗りつぶし矩形
+        device->Draw2DRect(
+            izanagi::CIntRect(100, 100, 200, 200),
+            IZ_COLOR_RGBA(0, 0xff, 0, 0xff));
+
+        // ライン
+        device->Draw2DLine(
+            izanagi::CIntPoint(100, 100),    // 始点
+            izanagi::CIntPoint(200, 200),    // 終点
+            IZ_COLOR_RGBA(0xff, 0, 0, 0xff));
+
+        device->End2D();
+    }
+}
+
+static const IZ_UINT BUF_SIZE = 1 * 1024 * 1024;
+static IZ_UINT8 BUF[BUF_SIZE];
+
+static const IZ_UINT GFX_BUF_SIZE = 1 * 1024 * 1024;
+static IZ_UINT GFX_BUF[GFX_BUF_SIZE];
+
+static const IZ_UINT SCREEN_WIDTH = 1280;
+static const IZ_UINT SCREEN_HEIGHT = 720;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    WaterDropApp app;
+
+    int ret = SampleMain(
+        hInstance,
+        &app,
+        "WaterDrop",
+        SCREEN_WIDTH, SCREEN_HEIGHT,
+        BUF, BUF_SIZE,
+        GFX_BUF, GFX_BUF_SIZE);
+
+    return ret;
+}
