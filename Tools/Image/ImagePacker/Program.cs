9034359e91e1bf3d5052221d528bcdf00ecd3a9c
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-11-25 15:59:04 +0000

    Add ImagePacker.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@877 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Tools/Image/ImagePacker/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImagePacker
+{
+    class Program
+    {
+        static void DisplayUsage()
+        {
+            Console.WriteLine("Usage : ImagePacker <options>");
+            Console.WriteLine("");
+            Console.WriteLine("Options");
+            Console.WriteLine(" -i <input>");
+            Console.WriteLine(" -d <directory>");
+            Console.WriteLine(" -o <output>");
+            Console.WriteLine(" -f <format>");
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Option
+    {
+        public string Directory;
+        public string Input;
+        public string Output;
+        public string Format;
+
+        public Option(string[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                var arg = args[i];
+
+                if (arg == "-i")
+                {
+                    this.Input = args[++i];
+                }
+                else if (arg == "-d")
+                {
+                    this.Directory = args[++i];
+                }
+                else if (arg == "-o")
+                {
+                    this.Output = args[++i];
+                }
+                else if (arg == "-f")
+                {
+                    this.Format = args[++i];
+                    this.Format = this.Format.ToLower();
+                }
+            }
+        }
+
+        public bool IsValid()
+        {
+            // TODO
+            return true;
+        }
+    }
+}
