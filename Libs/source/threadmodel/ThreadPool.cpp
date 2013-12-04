3e9485a79e4b1f8aa6914c110811604c012bff40
newvf91.brjtn579.dn@gmail.com <newvf91.brjtn579.dn@gmail.com@05d2ca3c-aac1-175a-b4cf-4be6c0a525a7>
2013-12-04 16:50:19 +0000

    Fix ThreadPool.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@884 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

--- Libs/source/threadmodel/ThreadPool.cpp
+++ Libs/source/threadmodel/ThreadPool.cpp
@@ -1,86 +1,227 @@
 #include "threadmodel/ThreadPool.h"
-#include "threadmodel/ThreadModelJobWorker.h"
-#include "threadmodel/ThreadModelTask.h"
 
 namespace izanagi
 {
 namespace threadmodel
 {
     IMemoryAllocator* CThreadPool::s_Allocator = IZ_NULL;
-    
-    IZ_UINT CThreadPool::s_WorkerNum = 0;
-    CJobWorker** CThreadPool::s_Workers = IZ_NULL;
-        
-    IZ_BOOL CThreadPool::s_IsTerminated = IZ_FALSE;
+    sys::CMutex CThreadPool::s_Mutex;
 
-    IZ_BOOL CThreadPool::Init(
+    IZ_UINT CThreadPool::s_MaxThreadNum = 0;
+    CStdList<CThreadPool::CThread> CThreadPool::s_ThreadList;
+
+    CThreadPool::CThread::CThread()
+    {
+        m_Sema.Init(0);
+        m_Event.Open();
+        m_Mutex.Open();
+
+        m_State = State_Waiting;
+
+        m_ListItem.Init(this);
+    }
+
+    void CThreadPool::CThread::SetRunnable(sys::IRunnable* runnable)
+    {
+        m_Mutex.Lock();
+        {
+            m_State = State_Registered;
+            m_Runnable = runnable;
+        }
+        m_Mutex.Unlock();
+
+        m_Event.Reset();
+        m_Sema.Release();
+    }
+
+    sys::IRunnable* CThreadPool::CThread::GetRunnable()
+    {
+        return m_Runnable;
+    }
+
+    void CThreadPool::CThread::Run()
+    {
+        while (m_Sema.Wait()) {
+            {
+                sys::CGuarder guard(m_Mutex);
+
+                if (m_State == State_WillFinish) {
+                    m_State = State_Finished;
+                    m_Event.Set();
+                    break;
+                }
+
+                m_State = State_Running;
+
+                if (m_Runnable != IZ_NULL) {
+                    m_Runnable->Run(GetUserData());
+                }
+
+                m_State = State_Waiting;
+            }
+
+            m_Event.Set();
+        }
+    }
+
+    void CThreadPool::CThread::Join()
+    {
+        // NOTE
+        // ここでは本当にJoinはせずに、Runが１周するのを待つ
+        m_Event.Wait();
+    }
+
+    void CThreadPool::CThread::Terminate()
+    {
+        {
+            sys::CGuarder guard(m_Mutex);
+
+            if (m_State == State_Finished) {
+                return;
+            }
+
+            m_State = State_WillFinish;
+        }
+
+        // 止まっているかもしれないので起こす
+        m_Sema.Release();
+
+        sys::CThread::Join();
+
+        m_Sema.Close();
+        m_Event.Close();
+        m_Mutex.Close();
+    }
+
+    IZ_BOOL CThreadPool::CThread::IsWaiting()
+    {
+        sys::CGuarder guard(m_Mutex);
+
+        return (m_State == State_Waiting);
+    }
+
+    void CThreadPool::Init(IMemoryAllocator* allocator)
+    {
+        Init(
+            allocator,
+            sys::CEnvironment::GetProcessorNum());
+    }
+
+    void CThreadPool::Init(
         IMemoryAllocator* allocator,
         IZ_UINT threadNum)
     {
-        VRETURN(!s_IsTerminated);
-
-        if (s_Workers != IZ_NULL) {
-            return IZ_TRUE;
+        if (s_Allocator != IZ_NULL) {
+            return;
         }
 
-        void* buf = ALLOC(
-            allocator, 
-            sizeof(CJobWorker*) * threadNum + sizeof(CJobWorker) * threadNum);
-        VRETURN(buf != IZ_NULL);
+        s_Mutex.Open();
 
-        IZ_UINT8* p = (IZ_UINT8*)buf;
-
-        s_Workers = reinterpret_cast<CJobWorker**>(p);
-        p += sizeof(CJobWorker*) * threadNum; 
-
-        for (IZ_UINT i = 0; i < threadNum; i++) {
-            s_Workers[i] = new(p) CJobWorker();
-            p += sizeof(CJobWorker);
-
-            s_Workers[i]->Start();
-        }
-
-        s_WorkerNum = threadNum;
         s_Allocator = allocator;
 
-        return IZ_TRUE;
+        s_MaxThreadNum = threadNum;
+
+        for (IZ_UINT i = 0; i < threadNum; i++) {
+            CThread* thread = CreateThread();
+
+            s_ThreadList.AddTail(thread->GetListItem());
+        }
     }
 
-    IZ_BOOL CThreadPool::Enqueue(CTask* task, IZ_BOOL deleteTaskWhenFinish/*= IZ_FALSE*/)
+    CThreadPool::CThread* CThreadPool::CreateThread()
     {
-        for (IZ_UINT i = 0; i < s_WorkerNum; i++) {
-            if (s_Workers[i]->IsWaiting()) {
-                // Taskを待機状態にセット
-                task->SetState(CJob::State_Waiting);
+        void* buf = ALLOC(s_Allocator, sizeof(CThread));
+        VRETURN_NULL(buf);
 
-                if (deleteTaskWhenFinish) {
-                    task->SetEnableDeleteWhenFinish();
-                }
+        CThread* ret = new(buf) CThread();
+        ret->Start();
 
-                s_Workers[i]->Register(task);
-                return IZ_TRUE;
-            }
-        }
-        return IZ_FALSE;
+        return ret;
     }
 
     void CThreadPool::Terminate()
     {
-        if (s_IsTerminated) {
-            return;
+        ListItem* item = s_ThreadList.GetTop();
+
+        while (item != IZ_NULL) {
+            CThread* thread = item->GetData();
+
+            ListItem* next = item->GetNext();
+
+            thread->Terminate();
+            delete thread;
+
+            FREE(s_Allocator, thread);
+
+            item = next;
         }
 
-        // ワーカーをJoinする
-        for (IZ_UINT i = 0; i < s_WorkerNum; i++) {
-            s_Workers[i]->Join();
-            delete s_Workers[i];
+        IZ_ASSERT(s_ThreadList.GetItemNum() == 0);
+
+        s_Mutex.Close();
+    }
+
+    CThreadPool::CThread* CThreadPool::GetThread(sys::IRunnable* runnable)
+    {
+        sys::CGuarder gurader(s_Mutex);
+
+        ListItem* item = s_ThreadList.GetTop();
+
+        while (item != IZ_NULL) {
+            CThread* thread = item->GetData();
+
+            if (thread->IsWaiting()) {
+                thread->SetRunnable(runnable);
+                return thread;
+            }
+
+            item = item->GetNext();
         }
 
-        FREE(s_Allocator, s_Workers);
-        s_Workers = IZ_NULL;
-        s_WorkerNum = 0;
+        return IZ_NULL;
+    }
 
-        s_IsTerminated = IZ_TRUE;
+    CThreadPool::CThread* CThreadPool::GetThreadUntilThreadIsEmpty(sys::IRunnable* runnable)
+    {
+        sys::CGuarder gurader(s_Mutex);
+
+        ListItem* item = s_ThreadList.GetTop();
+
+        while (item != IZ_NULL) {
+            CThread* thread = item->GetData();
+
+            if (thread->IsWaiting()) {
+                thread->SetRunnable(runnable);
+                return thread;
+            }
+
+            item = item->GetNext();
+        }
+
+        // ここに来た時点で空きスレッドが無いので待つ
+        item = s_ThreadList.GetTop();
+
+        while (item != IZ_NULL) {
+            CThread* thread = item->GetData();
+
+            if (!thread->IsWaiting()) {
+                // TODO
+                // とりあえず、最初に見つかった空いていないスレッドが空くのを待つ
+                thread->Join();
+            }
+
+            thread->SetRunnable(runnable);
+            return thread;
+        }
+
+        // ある？
+        IZ_ASSERT(IZ_FALSE);
+        return IZ_NULL;
+    }
+
+    IZ_UINT CThreadPool::GetMaxThreadNum()
+    {
+        return s_MaxThreadNum;
     }
 }   // namespace threadmodel
 }   // namespace izanagi
