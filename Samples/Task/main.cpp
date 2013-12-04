2f873dc31e00e4830d9f2a993839da37ddee1d2f
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-12-04 16:51:30 +0000

    Add Task sample.
    git-svn-id: https://izanagi.googlecode.com/svn/trunk/@887 05d2ca3c-aac1-175a-b4cf-4be6c0a525a7

---
+++ Samples/Task/main.cpp
@@ -0,0 +1,131 @@
+#include "izSampleKit.h"
+#include "izThreadModel.h"
+
+static IZ_INT Data[100];
+
+class CTask : public izanagi::threadmodel::CTask{
+public:
+    CTask() {}
+    virtual ~CTask() {}
+
+private:
+    virtual void OnRun()
+    {
+        for (IZ_UINT i = m_From; i < m_To; i++) {
+            IZ_PRINTF("Task %d\n", i);
+            Data[i] = i - 1;
+        }
+
+        izanagi::sys::CThread::Sleep(100);
+    }
+
+public:
+    void SetRange(IZ_UINT from, IZ_UINT to)
+    {
+        m_From = from;
+        m_To = to;
+    }
+
+private:
+    IZ_UINT m_From;
+    IZ_UINT m_To;
+};
+
+IZ_UINT8 buffer[1 * 1024 * 1024];
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    izanagi::CStandardMemoryAllocator allocator;
+    allocator.Init(sizeof(buffer), buffer);
+
+    izanagi::threadmodel::CTaskScheduler scheduler;
+
+    CTask tasks[4];
+    tasks[0].SetRange( 0,  25);
+    tasks[1].SetRange(25,  50);
+    tasks[2].SetRange(50,  75);
+    tasks[3].SetRange(75, 100);
+
+#if 0
+    // Sample1
+    // Simple Sample
+    {
+        izanagi::threadmodel::CThreadPool::Init(&allocator, 4);
+
+        for (IZ_UINT i = 0; i < COUNTOF(Data); i++) {
+            Data[i] = i;
+        }
+
+        for (IZ_UINT i = 0; i < COUNTOF(tasks); i++) {
+            scheduler.Enqueue(tasks[i], IZ_FALSE);
+        }
+
+        for (IZ_UINT i = 0; i < COUNTOF(tasks); i++) {
+            tasks[i].Wait();
+        }
+    
+        for (IZ_UINT i = 0; i < COUNTOF(Data); i++) {
+            if (Data[i] != i - 1) {
+                IZ_PRINTF("Failed Sample1 !!!\n");
+                IZ_ASSERT(IZ_FALSE);
+            }
+        }
+    }
+#elif 0
+    // Sample2
+    // If thread is not empty, task execute on main thread.
+    {
+        izanagi::threadmodel::CThreadPool::Init(&allocator, 1);
+
+        for (IZ_UINT i = 0; i < COUNTOF(Data); i++) {
+            Data[i] = i;
+        }
+
+        for (IZ_UINT i = 0; i < COUNTOF(tasks); i++) {
+            scheduler.Enqueue(tasks[i], IZ_FALSE);
+        }
+
+        for (IZ_UINT i = 0; i < COUNTOF(tasks); i++) {
+            tasks[i].Wait();
+        }
+    
+        for (IZ_UINT i = 0; i < COUNTOF(Data); i++) {
+            if (Data[i] != i - 1) {
+                IZ_PRINTF("Failed Sample2 !!!\n");
+                IZ_ASSERT(IZ_FALSE);
+            }
+        }
+    }
+#elif 1
+    // Sample3
+    // If thread is not empty, task wait for task is empty.
+    {
+        izanagi::threadmodel::CThreadPool::Init(&allocator, 1);
+
+        for (IZ_UINT i = 0; i < COUNTOF(Data); i++) {
+            Data[i] = i;
+        }
+
+        for (IZ_UINT i = 0; i < COUNTOF(tasks); i++) {
+            scheduler.Enqueue(tasks[i], IZ_TRUE);
+        }
+
+        for (IZ_UINT i = 0; i < COUNTOF(tasks); i++) {
+            tasks[i].Wait();
+        }
+    
+        for (IZ_UINT i = 0; i < COUNTOF(Data); i++) {
+            if (Data[i] != i - 1) {
+                IZ_PRINTF("Failed Sample3 !!!\n");
+                IZ_ASSERT(IZ_FALSE);
+            }
+        }
+    }
+#endif
+
+    izanagi::threadmodel::CThreadPool::Terminate();
+
+    allocator.Dump();
+
+    return 0;
+}
