1b2e570b2c43c330549d73c9a3456ac5b0a87113
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-12-28 03:07:56 +0900

    Add files for running makeitso.

---
+++ GenMakefile.bat
@@ -0,0 +1,4 @@
+External\makeitso\bin\MakeItSo.exe -file=Libs\project\vs2010\izanagi.sln -config=MakeItSo.config
+
+copy /Y /A Libs\project\vs2010\*.makefile Libs\project\makefile\
+copy /Y /A Libs\project\vs2010\Makefile Libs\project\makefile\
