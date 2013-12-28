a1bf337f6a7551b7d5fa30780eeb18530019f76a
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-12-28 14:10:40 +0900

    Fix copy option.

--- GenMakefile.bat
+++ GenMakefile.bat
@@ -1,4 +1,4 @@
 External\makeitso\output\MakeItSo.exe -file=Libs\project\vs2010\izanagi.sln -config=MakeItSo.config
 
-copy /Y /A Libs\project\vs2010\*.makefile Libs\project\makefile\
-copy /Y /A Libs\project\vs2010\Makefile Libs\project\makefile\
+copy /Y Libs\project\vs2010\*.makefile Libs\project\makefile\
+copy /Y Libs\project\vs2010\Makefile Libs\project\makefile\
