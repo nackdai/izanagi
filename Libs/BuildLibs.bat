b9c8e11cfb1c0b222ab168ffbd108cd29b14a454
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-12-31 13:02:39 +0900

    Add command for building tool.

--- Libs/BuildLibs.bat
+++ Libs/BuildLibs.bat
@@ -5,6 +5,15 @@ set GFX=%2
 
 if %GFX%==GLES2 (
     %MSBUILD% ..\External\ANGLE\src\ANGLE.sln /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=Win32 || goto error
+
+    cd ..\Tools
+    cd External
+    call BuildMojoShader.bat Release
+    cd ..
+    %MSBUILD% External\Preproc\Preproc_VC10.sln /t:%TARGET% /p:Configuration=Release || goto error
+    %MSBUILD% projects\vs2010\ShaderCompiler.sln /t:%TARGET% /p:Configuration=Release || goto error
+
+    cd ..\Libs
 )
 
 %MSBUILD% ..\External\freetype\builds\win32\vc2010\freetype.sln /t:%TARGET% /p:Configuration="%CONFIG% Multithreaded" || goto error
