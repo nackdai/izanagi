beef9f9dc6a697c84bd63533b470d7e09d0d2de3
nakdai <newvf91.brjtn579.dn@gmail.com>
2013-12-31 13:02:55 +0900

    Fix build order.

--- BuildAll.bat
+++ BuildAll.bat
@@ -1,13 +1,13 @@
 set CONFIG=%1
 set GFX=%2
 
-cd Libs
-call BuildLibs_%CONFIG%_%GFX%.bat
-cd ..
-
 cd Tools
 call BuildExternal_%CONFIG%.bat
 call BuildTools_%CONFIG%.bat
+cd ..
+
+cd Libs
+call BuildLibs_%CONFIG%_%GFX%.bat
 cd ..
 
 cd Samples
