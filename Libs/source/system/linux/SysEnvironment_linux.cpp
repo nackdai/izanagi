05563ede7687b2dad8066c15135a14c0bba363f5
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-01-07 01:08:06 +0900

    Rename typo file name.

---
+++ Libs/source/system/linux/SysEnvironment_linux.cpp
@@ -0,0 +1,97 @@
+#if 0
+#include <windows.h>
+#include <winbase.h>
+#include "system/SysEnvironment.h"
+
+namespace izanagi
+{
+namespace sys
+{
+    // Return CPU num.
+    IZ_UINT CEnvironment::GetProcessorNum()
+    {
+        SYSTEM_INFO info;
+        ::GetSystemInfo(&info);
+    
+        return info.dwNumberOfProcessors;
+    }
+
+    /**
+    * Get display's pixel width.
+    */
+    IZ_UINT CEnvironment::GetPixelWidth(void* nativeWndHandle)
+    {
+        HDC hdc = (HDC)nativeWndHandle;
+        IZ_ASSERT(hdc != IZ_NULL);
+        IZ_UINT ret = ::GetDeviceCaps(hdc, HORZRES);
+        return ret;
+    }
+
+    /**
+    * Get display's pixel height.
+    */
+    IZ_UINT CEnvironment::GetPixelHeight(void* nativeWndHandle)
+    {
+        HDC hdc = (HDC)nativeWndHandle;
+        IZ_ASSERT(hdc != IZ_NULL);
+        IZ_UINT ret = ::GetDeviceCaps(hdc, VERTRES);
+        return ret;
+    }
+
+    /**
+    * Get display' physical width.
+    */
+    IZ_UINT CEnvironment::GetPhysicalWidth(void* nativeWndHandle)
+    {
+        HDC hdc = (HDC)nativeWndHandle;
+        IZ_ASSERT(hdc != IZ_NULL);
+        IZ_UINT ret = ::GetDeviceCaps(hdc, HORZSIZE);
+        return ret;
+    }
+
+    /**
+    * Get display' physical height.
+    */
+    IZ_UINT CEnvironment::GetPhysicalHeight(void* nativeWndHandle)
+    {
+        HDC hdc = (HDC)nativeWndHandle;
+        IZ_ASSERT(hdc != IZ_NULL);
+        IZ_UINT ret = ::GetDeviceCaps(hdc, VERTSIZE);
+        return ret;
+    }
+
+    /**
+    * Get display's horizontal dpi.
+    */
+    IZ_UINT CEnvironment::GetHorizontalDPI(void* nativeWndHandle)
+    {
+        HDC hdc = (HDC)nativeWndHandle;
+        IZ_ASSERT(hdc != IZ_NULL);
+        IZ_UINT ret = ::GetDeviceCaps(hdc, LOGPIXELSX);
+        return ret;
+    }
+
+    /**
+    * Get display's vertical dpi.
+    */
+    IZ_UINT CEnvironment::GetVerticalDPI(void* nativeWndHandle)
+    {
+        HDC hdc = (HDC)nativeWndHandle;
+        IZ_ASSERT(hdc != IZ_NULL);
+        IZ_UINT ret = ::GetDeviceCaps(hdc, LOGPIXELSY);
+        return ret;
+    }
+
+    /**
+    * Get display's refresh rate.
+    */
+    IZ_UINT CEnvironment::GetRefreshRate(void* nativeWndHandle)
+    {
+        HDC hdc = (HDC)nativeWndHandle;
+        IZ_ASSERT(hdc != IZ_NULL);
+        IZ_UINT ret = ::GetDeviceCaps(hdc, VREFRESH);
+        return ret;
+    }
+}   // namespace sys
+}   // namespace izanagi
+#endif
