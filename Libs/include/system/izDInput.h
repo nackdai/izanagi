d4156223be4ae3141be61942f0e4a416e4f4d4e2
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-01-26 01:50:03 +0900

    Disable Pad at GLUT build.

--- Libs/include/system/izDInput.h
+++ Libs/include/system/izDInput.h
@@ -1,13 +1,16 @@
 #if !defined(__IZANAGI_SYSTEM_DINPUT_H__)
 #define __IZANAGI_SYSTEM_DINPUT_H__
 
-#ifndef DIRECTINPUT_VERSION
-#define DIRECTINPUT_VERSION 0x0800 
+#ifdef __IZ_DX9__
+
+    #ifndef DIRECTINPUT_VERSION
+        #define DIRECTINPUT_VERSION 0x0800 
+    #endif
+
+    #include <dinput.h>
+
+    #define D_INPUT         IDirectInput8
+    #define D_INPUT_DEVICE  IDirectInputDevice8
 #endif
-
-#include <dinput.h>
-
-#define D_INPUT         IDirectInput8
-#define D_INPUT_DEVICE  IDirectInputDevice8
 
 #endif  // #if !defined(__IZANAGI_SYSTEM_DINPUT_H__)
