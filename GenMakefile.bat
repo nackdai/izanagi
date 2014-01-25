25cf2c71f8325910df7b6b0936e4bedc1150536e
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-01-26 00:58:27 +0900

    Add code to build MakeItSo before generating makefiles.

--- GenMakefile.bat
+++ GenMakefile.bat
@@ -1,4 +1,19 @@
-External\makeitso\output\MakeItSo.exe -file=Libs\project\vs2010\izanagi.sln -config=MakeItSo.config
+set MSBUILD="C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+set MAKEITSO="External\makeitso\output\MakeItSo.exe"
+
+if not exist %MAKEITSO% (
+    %MSBUILD% External\makeitso\MakeItSo.sln /t:Build /p:Configuration=Release || goto error
+)
+
+%MAKEITSO% -file=Libs\project\vs2010\izanagi.sln -config=MakeItSo.config
 
 copy /Y Libs\project\vs2010\*.makefile Libs\project\makefile\
 copy /Y Libs\project\vs2010\Makefile Libs\project\makefile\
+
+@echo off
+exit /b 1
+
+:error
+@echo off
+echo "Error====="
+pause
