407a3aa48de481f9de17287e7a70abfec9ebd43c
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-02-13 23:56:41 +0900

    Rename files for making libs makefiles.

--- GenLibsMakefile.bat
+++ GenLibsMakefile.bat
@@ -5,7 +5,7 @@ if not exist %MAKEITSO% (
     %MSBUILD% External\makeitso\MakeItSo.sln /t:Build /p:Configuration=Release || goto error
 )
 
-%MAKEITSO% -file=Libs\project\vs2010\izanagi.sln -config=MakeItSo.config
+%MAKEITSO% -file=Libs\project\vs2010\izanagi.sln -config=MakeItSo_Libs.config
 
 copy /Y Libs\project\vs2010\*.makefile Libs\project\makefile\
 copy /Y Libs\project\vs2010\Makefile Libs\project\makefile\
