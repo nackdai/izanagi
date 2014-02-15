d5ef049233c69d39c225a2b58e194d3840a09493
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-02-15 12:53:24 +0900

    - Fix bat file to make makefile for Samples. - Add config file to make makefile for Samples.

--- GenSamplesMakefile.bat
+++ GenSamplesMakefile.bat
@@ -5,7 +5,7 @@ if not exist %MAKEITSO% (
     %MSBUILD% External\makeitso\MakeItSo.sln /t:Build /p:Configuration=Release || goto error
 )
 
-%MAKEITSO% -file=Samples\projects\vs2010\Samples.sln -config=MakeItSo_Libs.config
+%MAKEITSO% -file=Samples\projects\vs2010\Samples.sln -config=MakeItSo_Samples.config
 
 copy /Y Samples\projects\vs2010\*.makefile Samples\projects\makefile\
 copy /Y Samples\projects\vs2010\Makefile Samples\projects\makefile\
