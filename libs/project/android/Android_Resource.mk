6fdcb56990f60648daaed117416875788470db38
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-03-10 23:58:10 +0900

    - Update Android makefiles for Libs. - Remove unnecessary makefile.

--- libs/project/android/Android_Resource.mk
+++ libs/project/android/Android_Resource.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE     := libResource
-LOCAL_LDLIBS    := -llog -lGLESv2
+LOCAL_LDLIBS     := -llog -lGLESv2
 LOCAL_CFLAGS     := -DANDROID -D__IZ_GLES2__ -D_DEBUG -D_LIB -D__IZ_DEBUG__ -D__IZ_OGL__ -D__IZANAGI_NO_USE_D3D__
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include
 LOCAL_SRC_FILES  := ../../source/resource/Resource.cpp \
