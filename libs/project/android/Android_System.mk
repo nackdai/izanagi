ef37a111e72fef8d6f263c2781aebfc8c57eab6d
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-04-05 21:34:51 +0900

    Update makefile for android.

--- libs/project/android/Android_System.mk
+++ libs/project/android/Android_System.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE     := libSystem
 LOCAL_CFLAGS     := -DANDROID -D__IZ_GLES2__ -D_DEBUG -D_LIB -D__IZ_DEBUG__ -D__IZ_OGL__ -D__IZANAGI_NO_USE_D3D__
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../include
 LOCAL_SRC_FILES  := ../../source/system/android/SysWindow_android.cpp \
-../../source/system/linux/FileStream_linux.cpp \
+../../source/system/android/FileStream_android.cpp \
 ../../source/system/linux/SysEnvironment_linux.cpp \
 ../../source/system/linux/SysTimer_linux.cpp \
 ../../source/system/linux/SysUtil_linux.cpp \
