1d7fbfe3ffcb5d5144c6aa214448b6f354c72fa3
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-04-05 19:08:05 +0900

    Modiy file stream for win32.

--- libs/source/system/win32/FileStream_win32.cpp
+++ libs/source/system/win32/FileStream_win32.cpp
@@ -3,17 +3,49 @@
 namespace izanagi
 {
     // ファイルオープン
-    void CFileStream::OnOpen(const char* path, const char* mode)
+    void CFileStream::OnOpen(FILE_HANDLE* file, const char* path, const char* mode)
     {
-        fopen_s(&m_File, path, mode);
+        fopen_s(file, path, mode);
     }
 
-    void CFileStream::OnClose()
+    void CFileStream::OnClose(FILE_HANDLE file)
     {
-        if (m_File != IZ_NULL) {
-            fclose(m_File);
+        if (file != IZ_NULL) {
+            fclose(file);
         }
+    }
 
-        m_File = IZ_NULL;
+    IZ_INT CFileStream::OnSeek(FILE_HANDLE file, IZ_INT offset, IZ_INT origin)
+    {
+        return fseek(file, offset, origin);
+    }
+
+    IZ_LONG CFileStream::GetLength(FILE_HANDLE file)
+    {
+        // ファイルサイズ取得
+        fseek(file, 0, SEEK_END);
+        IZ_LONG ret = ftell(file);
+        fseek(file, 0, SEEK_SET);
+        return ret;
+    }
+
+    IZ_INT CFileStream::OnRead(void* buf, IZ_UINT size, IZ_UINT count, FILE_HANDLE file)
+    {
+        return fread(buf, size, count, file);
+    }
+
+    IZ_UINT CFileStream::Tell(FILE_HANDLE file)
+    {
+        return (IZ_UINT)ftell(file);
+    }
+
+    IZ_UINT CFileStream::OnWrite(const void* src, size_t size, size_t count, FILE_HANDLE file)
+    {
+        return fwrite(src, size, count, file);
+    }
+
+    IZ_INT CFileStream::Print(FILE_HANDLE file, const char* str)
+    {
+        return IZ_FPRINTF(file, "%s", str);
     }
 }
