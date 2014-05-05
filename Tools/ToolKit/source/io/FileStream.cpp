012c54168a5276b39cf82b1731c1db22ef255902
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-05-05 14:37:33 +0900

    Fix build error.

--- Tools/ToolKit/source/io/FileStream.cpp
+++ Tools/ToolKit/source/io/FileStream.cpp
@@ -1,26 +1,26 @@
-#include "izSystem.h"
+#include "system/io/FileStream.h"
 
 namespace izanagi
 {
     // ファイルオープン
-    void CFileStream::OnOpen(FILE_HANDLE* file, const char* path, const char* mode)
+    void CFileStream::OnOpen(IZ_FILE_HANDLE* file, const char* path, const char* mode)
     {
         fopen_s(file, path, mode);
     }
 
-    void CFileStream::OnClose(FILE_HANDLE file)
+    void CFileStream::OnClose(IZ_FILE_HANDLE file)
     {
         if (file != IZ_NULL) {
             fclose(file);
         }
     }
 
-    IZ_INT CFileStream::OnSeek(FILE_HANDLE file, IZ_INT offset, IZ_INT origin)
+    IZ_INT CFileStream::OnSeek(IZ_FILE_HANDLE file, IZ_INT offset, IZ_INT origin)
     {
         return fseek(file, offset, origin);
     }
 
-    IZ_LONG CFileStream::GetLength(FILE_HANDLE file)
+    IZ_LONG CFileStream::GetLength(IZ_FILE_HANDLE file)
     {
         // ファイルサイズ取得
         fseek(file, 0, SEEK_END);
@@ -29,22 +29,22 @@ namespace izanagi
         return ret;
     }
 
-    IZ_INT CFileStream::OnRead(void* buf, IZ_UINT size, IZ_UINT count, FILE_HANDLE file)
+    IZ_INT CFileStream::OnRead(void* buf, IZ_UINT size, IZ_UINT count, IZ_FILE_HANDLE file)
     {
         return fread(buf, size, count, file);
     }
 
-    IZ_UINT CFileStream::Tell(FILE_HANDLE file)
+    IZ_UINT CFileStream::Tell(IZ_FILE_HANDLE file)
     {
         return (IZ_UINT)ftell(file);
     }
 
-    IZ_UINT CFileStream::OnWrite(const void* src, size_t size, size_t count, FILE_HANDLE file)
+    IZ_UINT CFileStream::OnWrite(const void* src, size_t size, size_t count, IZ_FILE_HANDLE file)
     {
         return fwrite(src, size, count, file);
     }
 
-    IZ_INT CFileStream::Print(FILE_HANDLE file, const char* str)
+    IZ_INT CFileStream::Print(IZ_FILE_HANDLE file, const char* str)
     {
         return IZ_FPRINTF(file, "%s", str);
     }
