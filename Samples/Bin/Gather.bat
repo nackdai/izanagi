a1e6874982169db2c2fa878f344ac4da2ae7e9f0
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-07-15 01:59:58 +0900

    Refactoring Gather.bat.

--- Samples/Bin/Gather.bat
+++ Samples/Bin/Gather.bat
@@ -1,11 +1,35 @@
 @echo off
-for /f "tokens=*" %%a in ('dir /b ..\*') do if not %%a==Bin (if not exist .\%%a (mkdir .\%%a))
+for /f "tokens=*" %%a in ('dir /b ..\*') do (
+	if not %%a==Bin (
+		if not exist .\%%a (
+			mkdir .\%%a
+		)
+	)
+)
 
-for /f "tokens=*" %%a in ('dir /b ..\*') do for /f "tokens=*" %%b in ('dir /b/s ..\%%a\*.exe') do if not %%a==Bin (xcopy /y %%b .\%%a)
+for /f "tokens=*" %%a in ('dir /b ..\*') do (
+	for /f "tokens=*" %%b in ('dir /b/s ..\%%a\*.exe') do (
+		if not %%a==Bin (
+			xcopy /y %%b .\%%a
+		)
+	)
+)
 
-for /f "tokens=*" %%a in ('dir /b ..\*') do if not %%a==Bin (if not exist .\%%a\data (mkdir .\%%a\data))
+for /f "tokens=*" %%a in ('dir /b ..\*') do (
+	if not %%a==Bin (
+		if not exist .\%%a\data (
+			mkdir .\%%a\data
+		)
+	)
+)
 
-for /f "tokens=*" %%a in ('dir /b ..\*') do if not %%a==Bin (if exist ..\%%a\data (xcopy /y/e ..\%%a\data\* .\%%a\data\))
+for /f "tokens=*" %%a in ('dir /b ..\*') do (
+	if not %%a==Bin (
+		if exist ..\%%a\data (
+			xcopy /y/e ..\%%a\data\* .\%%a\data\
+		)
+	)
+)
 
 if exist .\SampleRunner\SampleRunner.exe (
 	xcopy /y .\SampleRunner\SampleRunner.exe .\
