5c578c719acca84bb13216350f8f9b5aa5286809
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-07-15 03:23:33 +0900

    - Add a bat file to gather tools. - Add a bat file to run tools.

---
+++ Tools/Bin/Gather.bat
@@ -0,0 +1,30 @@
+@echo off
+for /f "tokens=*" %%a in ('dir /b ..\tools\Release') do (
+	if %%~xa==.exe (
+		xcopy /y ..\tools\Release\%%a .\
+	) else if %%~xa==.dll (
+		xcopy /y ..\tools\Release\%%a .\
+	)
+)
+
+set Data=.\data
+
+if not exist %Data% (
+	mkdir %Data%
+)
+
+set MediaDir=..\..\Media
+set SampleDir=..\..\Samples
+
+xcopy /y %MediaDir%\Seymour.dae %Data%
+xcopy /y %SampleDir%\Archive\*.xml %Data%
+xcopy /y %SampleDir%\Shader\shader\* %Data%
+xcopy /y %SampleDir%\PostEffect\posteffect\* %Data%
+xcopy /y %MediaDir%\FontSample* %Data%
+xcopy /y %MediaDir%\mikachan.ttf %Data%
+
+xcopy /y %MediaDir%\EnvMap.xml %Data%
+xcopy /y %MediaDir%\*_uffizi_probe.* %Data%
+xcopy /y %MediaDir%\*_uffizi_cross.* %Data%
+
+xcopy /y %MediaDir%\HeightMap.png %Data%
---
+++ Tools/Bin/RunTools.bat
@@ -0,0 +1,31 @@
+set MediaDir=..\..\Media
+set OutDir=.\out
+
+if not exist %OutDir% (
+	mkdir %OutDir%
+)
+
+ModelConverter.exe -i ".\data\Seymour.dae" -mtx 48 -e mdl -o "%OutDir%\Seymour.mdl"
+ModelConverter.exe -i ".\data\Seymour.dae" -mtx 48 -e msh -o "%OutDir%\Seymour.msh"
+ModelConverter.exe -i ".\data\Seymour.dae" -mtx 48 -e skl -o "%OutDir%\Seymour.skl"
+
+MaterialConverter.exe -i ".\data\Seymour.dae" -o "%OutDir%\Seymour.mtrl"
+
+AnimationConverter.exe -i ".\data\Seymour.dae" -o "%OutDir%\Seymour.anm"
+
+ArchiveConverter.exe -i ".\data\Archive.xml" -o "%OutDir%\Archive.arc"  -c ".\data\Config.xml" -d "%MediaDir%" -td "." -h
+
+ShaderConverter.exe -src ".\data\PhongShader.xml" -obj ".\shader_obj" -o "%OutDir%"
+
+PostEffectConverter.exe -src ".\data\PostEffectHDR.fx" -obj ".\posteffect_obj" -asm -o "%OutDir%\PostEffectHDR.pes" -I "..\PostEffect/PostEffectShader/include" -co /nologo -c "%DXSDK_DIR%Utilities\Bin\x86\fxc"
+
+FontConverter.exe -i ".\data\FontSample_Utf8.list" -o "%OutDir%\FontSample_Utf8.fnt" -aa 4 -c utf8 -ft ".\data\mikachan.ttf"
+
+ImageBuilder.exe -i ".\EnvMap.xml" -o "%OutDir%\EnvMap.img"
+
+EnvMapConverter.exe -i ".\data\uffizi_probe.png" -o "%OutDir%\MirrorMap.png" -ie c -oe m
+EnvMapConverter.exe -i ".\data\uffizi_probe.png" -o "%OutDir%\LatiLong.png" -ie c -oe l
+EnvMapConverter.exe -i ".\data\uffizi_probe.png" -o "%OutDir%\Angular.png" -ie c -oe a
+EnvMapConverter.exe -i ".\data\uffizi_probe.png" -o "%OutDir%\Cross.png" -ie c -oe cr
+
+:NormalMapMaker.exe -i ".\data\HeightMap.png" -o "%OutDir%\NormalMap.png"
