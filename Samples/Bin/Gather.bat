d3e81782e87a25d8dd8285536d1ae6fc5f802ebf
nakdai <newvf91.brjtn579.dn@gmail.com>
2014-07-15 01:23:30 +0900

    Modify the bat file to move SampleRunner.exe.

--- Samples/Bin/Gather.bat
+++ Samples/Bin/Gather.bat
@@ -6,3 +6,8 @@ for /f "tokens=*" %%a in ('dir /b ..\*') do for /f "tokens=*" %%b in ('dir /b/s 
 for /f "tokens=*" %%a in ('dir /b ..\*') do if not %%a==Bin (if not exist .\%%a\data (mkdir .\%%a\data))
 
 for /f "tokens=*" %%a in ('dir /b ..\*') do if not %%a==Bin (if exist ..\%%a\data (xcopy /y/e ..\%%a\data\* .\%%a\data\))
+
+if exist .\SampleRunner\SampleRunner.exe (
+	xcopy /y .\SampleRunner\SampleRunner.exe .\
+	rd /s/q .\SampleRunner
+)
