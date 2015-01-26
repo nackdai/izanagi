3f871b0718e49959845c3da60caf5adfdbed337f
nakdai <newvf91.brjtn579.dn@gmail.com>
2015-01-27 00:15:00 +0900

    Set root for python.

--- Libs/project/web/PreBuild.bat
+++ Libs/project/web/PreBuild.bat
@@ -6,3 +6,11 @@ PATH=%PATH%;%Emscripten%%Version%
 IF NOT EXIST ../lib (
     mkdir ../lib
 )
+
+for /F "usebackq"  %%t in (`where python`) do set PYTHON_ROOT=%%t
+
+if ""%PYTHON_ROOT%""=="""" (
+	set PYTHON_ROOT=..\..\..\External\emscripten\python\2.7.5.3_64bit
+)
+
+PATH=%PATH%;%PYTHON_ROOT%
