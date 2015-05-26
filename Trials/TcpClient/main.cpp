67c13e870c25477a756602718281ab14d3834538
nakdai <newvf91.brjtn579.dn@gmail.com>
2015-05-27 00:09:19 +0900

    Add tcp/ip network trial.

---
+++ Trials/TcpClient/main.cpp
@@ -0,0 +1,60 @@
+// http://www.geekpage.jp/programming/winsock/
+// http://www.geocities.jp/kandou_quester/Knowledge/Programming/Win32/_minimum_socket.html
+// http://yonex1.cis.ibaraki.ac.jp/~yonekura/2002kadai/lecture03.html
+
+// http://maverickproj.web.fc2.com/Network_02.html
+
+#include <WinSock2.h>
+#include <stdio.h>
+
+#pragma comment( lib, "WS2_32.lib" )
+
+int main(int argc, char** argv)
+{
+    WSADATA wsaData;
+
+    WSAStartup(
+        MAKEWORD(2,0),  // 利用するバージョン番号
+        &wsaData);      // 初期化データの保存先
+
+    SOCKET sock = socket(
+        AF_INET,        // アドレスファミリ
+        SOCK_STREAM,    // ソケットタイプ
+        0);             // プロトコル
+    if (sock == INVALID_SOCKET) {
+        printf("socket failed\n");
+        return 1;
+    }
+
+    int port = 8000;
+    const char* ip = "127.0.0.1";
+
+    sockaddr_in serverAddr;
+    {
+        // PF は protocol family の略で INET は "internetwork (UDP or TCP)" を意味する (ws2def.h 参照)
+        serverAddr.sin_family = PF_INET;
+        serverAddr.sin_port = htons(port);
+        serverAddr.sin_addr.S_un.S_addr = inet_addr(ip);
+    }
+
+	// 相手に接続
+    connect(
+        sock,   // 受け入れるソケット
+        (sockaddr*)&serverAddr,
+        sizeof(serverAddr));
+
+    // 送信
+    const char* buf = "test";
+    send(
+        sock,
+        buf,
+        strlen(buf),
+        0);
+
+    // ソケットを閉じる
+    closesocket(sock);
+
+    WSACleanup();
+
+    return 0;
+}
---
+++ Trials/TcpServer/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+// http://dobon.net/vb/dotnet/internet/tcpclientserver.html
+
+namespace TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Parse("127.0.0.1");
+
+            var listener = new TcpListener(ip, 8000);
+
+            listener.Start();
+            Console.WriteLine(
+                "Listenを開始しました({0}:{1})。",
+               ((IPEndPoint)listener.LocalEndpoint).Address,
+               ((IPEndPoint)listener.LocalEndpoint).Port);
+
+            for (; ; )
+            {
+                var connection = listener.AcceptTcpClient();
+
+                //NetworkStreamを取得
+                var ns = connection.GetStream();
+
+                //読み取り、書き込みのタイムアウトを10秒にする
+                //デフォルトはInfiniteで、タイムアウトしない
+                //(.NET Framework 2.0以上が必要)
+                ns.ReadTimeout = 10000;
+                ns.WriteTimeout = 10000;
+
+                byte[] receiveData = new byte[1000];
+                int size = ns.Read(receiveData, 0, receiveData.Length);
+                string receiveString = Encoding.UTF8.GetString(receiveData, 0, size);
+                Console.WriteLine("受信完了!");
+                Console.WriteLine("受信文字列 = {0}\n", receiveString);
+            }
+        }
+    }
+}
