0d12f813defb1ad1a93d42ac984403850204be1d
nakdai <newvf91.brjtn579.dn@gmail.com>
2015-05-28 00:03:32 +0900

    Add UDP server trial but not implement.

---
+++ Trials/Network/UdpServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UdpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var localAddr = IPAddress.Parse("127.0.0.1");
+            int port = 8000;
+
+            var ep = new IPEndPoint(localAddr, port);
+
+            var udp = new UdpClient(ep);
+
+            for (; ; )
+            {
+
+            }
+        }
+    }
+}
