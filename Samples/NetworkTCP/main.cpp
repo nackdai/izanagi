eccaa79b5f99861d95b6ed11345546e88c3dd1d3
nakdai <newvf91.brjtn579.dn@gmail.com>
2015-08-09 14:46:49 +0900

    It is enabled to connect as a client.

--- Samples/NetworkTCP/main.cpp
+++ Samples/NetworkTCP/main.cpp
@@ -3,18 +3,15 @@
 
 IZ_UINT8 buffer[2 * 1024 * 1024];
 
-IzMain(0, 0)
+void runAsServer(izanagi::IMemoryAllocator* allocator)
 {
-	izanagi::CStandardMemoryAllocator allocator;
-	allocator.Init(sizeof(buffer), buffer);
-
-	izanagi::net::Network::begin();
-
 	izanagi::net::IPv4Address addr;
+	addr.setHost("localhost");
+
 	izanagi::net::IPv4Endpoint ep(addr, 30000);
 
 	izanagi::net::Tcp tcp;
-	tcp.start(&allocator, ep, 1);
+	tcp.startAsServer(allocator, ep, 1);
 
 	IZ_BOOL willQuit = IZ_FALSE;
 
@@ -36,11 +33,53 @@ IzMain(0, 0)
 
 	tcp.stop();
 
+	th.join();
+}
+
+void runAsClient(izanagi::IMemoryAllocator* allocator)
+{
+	izanagi::net::IPv4Address addr;
+	addr.setHost("localhost");
+
+	izanagi::net::IPv4Endpoint ep(addr, 30000);
+
+	izanagi::net::Tcp tcp;
+	tcp.startAsClient(allocator, ep, IZ_FALSE);
+
+	static const IZ_CHAR* str = "test";
+
+	tcp.sendData(str, strlen(str));
+
+	// TODO
+	// ‘—M‚³‚ê‚é‚Ü‚Å‘Ò‚Â....
+	izanagi::sys::CThread::Sleep(1000);
+
+	tcp.stop();
+}
+
+IzMain(0, 0)
+{
+	IZ_BOOL isServer = IZ_FALSE;
+
+	if (argc > 0 && strcmp(argv[1], "s" ) == 0) {
+		isServer = IZ_TRUE;
+	}
+
+	izanagi::CStandardMemoryAllocator allocator;
+	allocator.Init(sizeof(buffer), buffer);
+
+	izanagi::net::Network::begin();
+
+	if (isServer) {
+		runAsServer(&allocator);
+	}
+	else {
+		runAsClient(&allocator);
+	}
+
 	izanagi::net::Network::end();
 
 	allocator.Dump();
-
-	th.join();
 
 	return 0;
 }
