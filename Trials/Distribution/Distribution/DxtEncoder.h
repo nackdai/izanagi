af2ca69505a9334b3e0c5f963fcb80b1e0d70459
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-02-05 14:06:48 +0900

    Add enet.

---
+++ Trials/Distribution/Distribution/DxtEncoder.h
@@ -0,0 +1,18 @@
+#if !defined(__DXT_ENCODER_H__)
+#define __DXT_ENCODER_H__
+
+#include "izSampleKit.h"
+
+class DxtEncoder
+{
+public:
+    DxtEncoder() {}
+    ~DxtEncoder();
+
+public:
+
+private:
+    
+};
+
+#endif    // #if !defined(__DXT_ENCODER_H__)
