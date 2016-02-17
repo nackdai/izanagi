2c4acbfa0c7ee55c0a5d9c6c06477adf7cff082e
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-02-17 18:23:29 +0900

    Simplify code.

--- Samples/SampleKit/SampleWindowProc.cpp
+++ Samples/SampleKit/SampleWindowProc.cpp
@@ -126,6 +126,7 @@ void CSampleWndProc::OnIdle()
 {
     IZ_ASSERT(m_App != IZ_NULL);
 
+#if 0
     m_App->GetTimer(0).Begin();
     m_App->GetTimer(1).Begin();
 
@@ -137,6 +138,9 @@ void CSampleWndProc::OnIdle()
     m_App->Present();
 
     m_App->GetTimer(0).End();
+#else
+    m_App->Idle();
+#endif
 }
 
 void CSampleWndProc::OnInit(const izanagi::sys::WindowHandle& handle)
