4290300e9faecea1256213b71575d55e99275e1e
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-03-02 20:54:20 +0900

    Modify for building with VS2013.

--- Libs/BuildLibs.bat
+++ Libs/BuildLibs.bat
@@ -4,50 +4,41 @@ set CURDIR=%CD%
 
 cd /d %~dp0
 
-set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
+if not defined DXSDK_DIR (
+    echo "There is no DX SDK"
+    goto error
+)
+
+call ..\Build\SetBuildParams.bat
+
 set TARGET=Build
 set CONFIG=%1
 set GFX=%2
 
+set TOOL_CONFIG=Release
+
+rem ShaderCompiler ================================
+call ..\Tools\External\BuildMojoShader.bat %TOOL_CONFIG%
+%MSBUILD% ..\Tools\External\Preproc\Preproc.sln /t:%TARGET% /p:Configuration=%TOOL_CONFIG% || goto error
+%MSBUILD% ..\Tools\projects\vs2013\ShaderCompiler.sln /t:%TARGET% /p:Configuration=%TOOL_CONFIG% || goto error
+
 if %GFX%==GLES2 (
     %MSBUILD% ..\External\ANGLE\projects\src\ANGLE.sln /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=Win32 || goto error
-
-    cd ..\Tools
-    cd External
-    call BuildMojoShader.bat Release
-    cd ..
-    %MSBUILD% External\Preproc\Preproc.sln /t:%TARGET% /p:Configuration=Release || goto error
-    %MSBUILD% projects\vs2013\ShaderCompiler.sln /t:%TARGET% /p:Configuration=Release || goto error
-
-    cd ..\Libs
 )
 
 if %GFX%==OGL (
-    %MSBUILD% ..\External\glew\glew.sln /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=Win32 || goto error
-    %MSBUILD% ..\External\freeglut\freeglut\freeglut\freeglut.sln /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=Win32 || goto error
-
-    cd ..\Tools
-    cd External
-    call BuildMojoShader.bat Release
-    cd ..
-    %MSBUILD% External\Preproc\Preproc.sln /t:%TARGET% /p:Configuration=Release || goto error
-    %MSBUILD% projects\vs2013\ShaderCompiler.sln /t:%TARGET% /p:Configuration=Release || goto error
-
-    cd ..\Libs
-
-    set GFX=OGL
+    call ..\External\BuildExternalOGL.bat
 )
 
-%MSBUILD% ..\External\freetype\builds\freetype.sln /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=Win32 || goto error
-%MSBUILD% ..\External\libuv\uv.sln /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=Win32 || goto error
+call ..\External\BuildExternal.bat
 
 %MSBUILD% project\vs2013\izanagi.sln /t:%TARGET% /p:Configuration=%CONFIG%_%GFX% /p:Platform=Win32 || goto error
 
-set CURDIR=%CD%
+cd /d %CURDIR%
 
 exit /b 1
 
 :error
-set CURDIR=%CD%
+cd /d %CURDIR%
 echo "Error====="
 pause
