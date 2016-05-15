4d0adb8d0839ee0e51f13e54fc6f31aa84f533fe
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-05-15 16:15:03 +0900

    Modify directory to export cmake result.

--- Tools/BuildExternal.bat
+++ Tools/BuildExternal.bat
@@ -8,23 +8,31 @@ set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 set TARGET=Rebuild
 set CONFIG=%1
 
-if not exist "External\squish\squish.sln" (
-    cd External\squish
-    ..\..\..\External\cmake\bin\cmake.exe -G "Visual Studio 12 2013"
-    cd ..\..
+if not exist "External\squish\build" (
+    mkdir External\squish\build
 )
 
-if not exist "External\assimp\Assimp.sln" (
-    cd External\assimp
-    ..\..\..\External\cmake\bin\cmake.exe -G "Visual Studio 12 2013"
-    cd ..\..
+if not exist "External\squish\build\squish.sln" (
+    cd External\squish\build
+    ..\..\..\..\External\cmake\bin\cmake.exe -G "Visual Studio 12 2013" ..\
+    cd ..\..\..
+)
+
+if not exist "External\assimp\build" (
+    mkdir External\assimp\build
+)
+
+if not exist "External\assimp\build\Assimp.sln" (
+    cd External\assimp\build
+    ..\..\..\..\External\cmake\bin\cmake.exe -G "Visual Studio 12 2013" ..\
+    cd ..\..\..
 )
 
 %MSBUILD% External\Preproc\Preproc.sln /t:%TARGET% /p:Configuration=%CONFIG% || goto error
 %MSBUILD% External\NvTriStrip\src\NvTriStrip\NvTriStrip.sln /t:%TARGET% /p:Configuration=%CONFIG% || goto error
 %MSBUILD% External\xerces\projects\Win32\VC12\xerces-all\xerces-all.sln /t:XercesLib:%TARGET% /p:Configuration=%CONFIG% || goto error
-%MSBUILD% External\squish\squish.sln /t:squish:%TARGET% /p:Configuration=%CONFIG% || goto error
-%MSBUILD% External\assimp\Assimp.sln /t:assimp:%TARGET% /p:Configuration=%CONFIG% || goto error
+%MSBUILD% External\squish\build\squish.sln /t:squish:%TARGET% /p:Configuration=%CONFIG% || goto error
+%MSBUILD% External\assimp\build\Assimp.sln /t:assimp:%TARGET% /p:Configuration=%CONFIG% || goto error
 
 cd External
 call BuildMojoShader.bat %CONFIG%
