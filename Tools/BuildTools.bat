86a653a8e48f1e2f94ddb18b218c0e98d882a23a
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-05-15 15:21:37 +0900

    Fix to build.

--- Tools/BuildTools.bat
+++ Tools/BuildTools.bat
@@ -8,7 +8,8 @@ set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 set TARGET=Build
 set CONFIG=%1
 
-rem %MSBUILD% ..\Libs\project\vs2013\Math.vcxproj /t:%TARGET% /p:Configuration=%CONFIG%_DX9 || goto error
+%MSBUILD% ..\Libs\project\vs2013\Math.vcxproj /t:%TARGET% /p:Configuration=%CONFIG%_DX9 || goto error
+%MSBUILD% Model\FbxImporter\project\vs2013/FbxImporter.sln /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=Win32 || goto error
 %MSBUILD% projects\vs2013\Tools.sln /t:%TARGET% /p:Configuration=%CONFIG% || goto error
 
 cd /d %CURDIR%
