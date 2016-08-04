2c348b1e2bb279daa087ce31dc1a9054eb22275b
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-08-04 14:45:32 +0900

    - Add a function to set left, right, top, bottom fov. - Compute projection matrix with left, right, top, bottom at near plane.

--- Libs/include/scenegraph/StereoCamera.h
+++ Libs/include/scenegraph/StereoCamera.h
@@ -7,6 +7,51 @@
 #include "VectorCamera.h"
 
 namespace izanagi {
+    struct HmdInfo {
+        IZ_FLOAT HResolution{ 1280.0f };            // HMD horizontal resolution in pixels
+        IZ_FLOAT VResolution{ 800.0f };             // HMD vertical resolution in pixels
+        IZ_FLOAT HScreenSize{ 0.14976f };           // HMD horizontal size in meters. 0.14976[m].
+        IZ_FLOAT VScreenSize{ 0.09356f };           // HMD vertical size in meters. 0.09356f[m].
+        IZ_FLOAT VScreenCenter{ 0.04678f };         // HMD screen center in meters. 0.04678f[m].
+        IZ_FLOAT EyeToScreenDistance{ 0.041f };     // HMD distance between eye and display in meters. 0.041f[m]
+        IZ_FLOAT LensSeparationDistance{ 0.0635f }; // HMD lens separation distance in meters. 0.0635f[m].
+        IZ_FLOAT InterpupillaryDistance{ 0.064f };  // HMD IPD (distance between pupils) in meters. 0.064f[m].
+        IZ_FLOAT DistortionK[4];                    // HMD lens distortion constants
+        IZ_FLOAT ChromaAbCorrection[4];             // HMD chromatic aberration correction parameters
+
+        HmdInfo()
+        {
+            DistortionK[0] = 1.0f;
+            DistortionK[1] = 0.22f;
+            DistortionK[2] = 0.24f;
+            DistortionK[3] = 0.0f;
+
+            ChromaAbCorrection[0] = 0.996f;
+            ChromaAbCorrection[1] = -0.004f;
+            ChromaAbCorrection[2] = 1.014f;
+            ChromaAbCorrection[3] = 0.0f;
+        }
+    };
+
+    // NOTE
+    // a typical DK1 half-FOV is 46 degrees inwards, 53 degrees outwards
+    // https://developer.oculusvr.com/forums/viewtopic.php?p=111699#p111699
+    //
+    // HFOV = 109 VFOV = 112.5
+    // https://forums.oculus.com/community/discussion/19476/oculus-dk1-fov
+    // 
+    // values for the DK2's default fov
+    // 106.19 degrees vertical 
+    // 95.06 degrees horizontal
+    // 47.53 degrees to the side and 46.63 degrees towards the nose
+    // https://forums.oculus.com/community/discussion/23715/fov-of-the-oculus-rift-dk2
+    struct FovPort {
+        IZ_FLOAT tanLeft;
+        IZ_FLOAT tanRight;
+        IZ_FLOAT tanTop;
+        IZ_FLOAT tanBottom;
+    };
+
     /**
      */
     class StereoCamera {
@@ -15,15 +60,22 @@ namespace izanagi {
         ~StereoCamera();
 
     public:
-
         enum Eye {
             Left,
             Right,
         };
 
+        static void setFov(
+            Eye type,
+            IZ_FLOAT degreeInwardsHFov,
+            IZ_FLOAT degreeOutwardsHFov,
+            IZ_FLOAT degfeeVFov,
+            FovPort& fov);
+
         static void getCamera(
             Eye type,
-            IZ_FLOAT ipd,   // 瞳孔間距離
+            const HmdInfo& info,
+            const FovPort& fov,
             CVectorCamera& dstCamera,
             const CVectorCamera& srcCamera);
     };
--- Libs/source/scenegraph/StereoCamera.cpp
+++ Libs/source/scenegraph/StereoCamera.cpp
@@ -1,15 +1,57 @@
 #include "scenegraph/StereoCamera.h"
 
 namespace izanagi {
+    void StereoCamera::setFov(
+        Eye type,
+        IZ_FLOAT degreeInwardsHFov,
+        IZ_FLOAT degreeOutwardsHFov,
+        IZ_FLOAT degfeeVFov,
+        FovPort& fov)
+    {
+        IZ_FLOAT in = IZ_DEG2RAD(degreeInwardsHFov);
+        IZ_FLOAT out = IZ_DEG2RAD(degreeOutwardsHFov);
+        IZ_FLOAT v = IZ_DEG2RAD(degfeeVFov);
+
+        fov.tanTop = tanf(v * 0.5f);
+        fov.tanBottom = -fov.tanTop;
+
+        //  -------+-------
+        //  l  | r   l | r
+        //     |       |
+        // out *  in   * out
+        //     L       R
+
+        if (type == Eye::Left) {
+            // Left.
+            fov.tanRight = tanf(in);    // inwards.
+            fov.tanLeft = -tanf(out);   // outwards.
+        }
+        else {
+            // Right.
+            fov.tanRight = tanf(out);   // outwards.
+            fov.tanLeft = -tanf(in);    // inwards.
+        }
+    }
+
     void StereoCamera::getCamera(
         Eye type,
-        IZ_FLOAT ipd,   // 瞳孔間距離
+        const HmdInfo& info,
+        const FovPort& fov,
         CVectorCamera& dstCamera,
         const CVectorCamera& srcCamera)
     {
         math::CVector4 dir(srcCamera.m_X);
 
-        auto t = ipd * 0.5f;
+        IZ_FLOAT ipd = info.InterpupillaryDistance;
+
+        // 投影スクリーンサイズに対する比率.
+        //IZ_FLOAT ratio = info.HResolution / info.HScreenSize;
+        IZ_FLOAT ratio = 1.0f;
+
+        // 物理的な瞳孔間距離から3D空間内の距離に変換する.
+        auto virtualIpd = ratio * ipd;
+
+        auto t = virtualIpd * 0.5f;
 
         // NOTE
         // Left Hand Coord.
@@ -33,15 +75,86 @@ namespace izanagi {
         math::SVector4::Copy(dstCamera.m_Z, srcCamera.m_Z);
         math::SVector4::Copy(dstCamera.m_Pos, pos);
 
+        // W2Vのみ計算.
         dstCamera.m_IsDirtyW2V = IZ_TRUE;
-        dstCamera.m_IsDirtyV2C = IZ_TRUE;
+        dstCamera.m_IsDirtyV2C = IZ_FALSE;
         dstCamera.m_NeedUpdateByTransform = IZ_TRUE;
-
-        dstCamera.m_Param.verticalFOV = srcCamera.GetParam().verticalFOV;
-        dstCamera.m_Param.aspect = srcCamera.GetParam().aspect;
-        dstCamera.m_Param.cameraNear = srcCamera.GetParam().cameraNear;
-        dstCamera.m_Param.cameraFar = srcCamera.GetParam().cameraFar;
-
         dstCamera.Update();
+
+        // NOTE
+        // projection matrix.
+        // l : left at near plane.
+        // r : right at near plane.
+        // t : top at near plane.
+        // b : bottom at near plane.
+        // n : z near.
+        // f : z far.
+        //
+        // D3DXMatrixPerspectiveOffCenterLH
+        // https://msdn.microsoft.com/ja-jp/library/cc372889.aspx
+        //    2n/(r-l)        0          0      0
+        //       0         2n/(t-b)      0      0
+        //  (l+r)/(l-r)  (t+b)/(b-t)  f/(f-n)   1
+        //       0            0       nf/(n-f)  0
+        //
+        // D3DXMatrixPerspectiveOffCenterRH
+        // https://msdn.microsoft.com/ja-jp/library/cc372890.aspx
+        //    2n/(r-l)        0          0      0
+        //       0         2n/(t-b)      0      0
+        //  (l+r)/(r-l)  (t+b)/(t-b)  f/(n-f)   -1
+        //       0            0       nf/(n-f)  0
+
+        // left_tangent   = l/n
+        // right_tangent  = r/n
+        // top_tangent    = t/n
+        // bottom_tangent = b/n
+        //
+        // right_tangent - left_tangent = r/n - l/n = (r-l)/n
+        //  => (r-l)/2n = (right_tangent - left_tangent)/2
+        //  => 2n/(r-l) = 2/(right_tangent - left_tangent)
+        // 
+        // 2n/(t-b) = 2/(top_tangent - bottom_tangent)
+        //
+        // left_tangent + right_tangent = l/n + r/n = (l+r)/n
+        //
+        // (l+r)/(l-r)
+        //   = (l+r)/n * 1/2 * -2n/(r-l)
+        //   = - (left_tangent + right_tangent) * 1/2 * 2/(right_tangent - left_tangent)
+        //   = - (left_tangent + right_tangent) / (right_tangent - left_tangent)
+        //
+        // top_tangent + bottom_tangent = t/n + b/n = (t+b)/n
+        // 
+        // (t+b)/(b-t)
+        //   = (t+b)/n * 1/2 * -2n(t-b)
+        //   = - (top_tangent + bottom_tangent) * 1/2 * 2/(top_tangent - bottom_tangent)
+        //   = - (top_tangent + bottom_tangent) / (top_tangent - bottom_tangent)
+
+        // V2Cを計算.
+        auto& mtxV2C = const_cast<math::SMatrix44&>(dstCamera.GetParam().mtxV2C);
+        {
+            IZ_FLOAT f = srcCamera.GetParam().cameraFar;
+            IZ_FLOAT n = srcCamera.GetParam().cameraNear;
+
+            math::SMatrix44::SetUnit(mtxV2C);
+
+            mtxV2C.m[0][0] = 2.0f / (fov.tanRight - fov.tanLeft);   // 2n/(r-l)
+            mtxV2C.m[1][1] = 2.0f / (fov.tanTop - fov.tanBottom);   // 2n/(t-b)
+
+            mtxV2C.m[2][0] = (fov.tanLeft + fov.tanRight) / (fov.tanLeft - fov.tanRight);
+            mtxV2C.m[2][1] = (fov.tanTop + fov.tanBottom) / (fov.tanBottom - fov.tanTop);
+
+            mtxV2C.m[2][2] = f / (f - n);
+            mtxV2C.m[2][3] = 1.0f;
+
+            mtxV2C.m[3][2] = n * f / (n - f);
+
+            mtxV2C.m[3][3] = 0.0f;
+        }
+
+        auto& mtxW2C = const_cast<math::SMatrix44&>(dstCamera.GetParam().mtxW2C);
+        math::SMatrix44::Mul(
+            mtxW2C,
+            dstCamera.GetParam().mtxW2V,
+            mtxV2C);
     }
 }
