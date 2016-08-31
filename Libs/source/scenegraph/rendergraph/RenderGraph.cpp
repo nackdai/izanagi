a15de8f39c0edc97515a74f51ad6ae5fb2be14a3
nakdai <newvf91.brjtn579.dn@gmail.com>
2016-08-31 15:55:01 +0900

    Modify assertion.

--- Libs/source/scenegraph/rendergraph/RenderGraph.cpp
+++ Libs/source/scenegraph/rendergraph/RenderGraph.cpp
@@ -88,7 +88,7 @@ namespace {
         // そのため、どちらでも 0 - 1 の範囲に正規化する必要がある
         //normalizedZ = (normalizedZ + 1.0f) * 0.5f;    // For OpenGL
 
-        IZ_ASSERT(normalizedZ >= 0.0f);
+        IZ_ASSERT(0.0f <= normalizedZ && normalizedZ <= 1.0f);
 
         return normalizedZ;
     }
