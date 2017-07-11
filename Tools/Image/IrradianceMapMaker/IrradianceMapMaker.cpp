2b31baf507af3d09ec722492f73df7bf6254006d
nakdai <newvf91.brjtn579.dn@gmail.com>
2017-07-11 10:53:57 +0900

    Implement IrradianceMapMaker to compute lambert.

--- Tools/Image/IrradianceMapMaker/IrradianceMapMaker.cpp
+++ Tools/Image/IrradianceMapMaker/IrradianceMapMaker.cpp
@@ -10,6 +10,8 @@
 #include "izToolKit.h"
 #include "izMath.h"
 
+#include "xorshift.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -52,7 +54,7 @@ public:
         const izanagi::math::SVector3& clr,
         float u, float v);
 
-    izanagi::math::SVector3 convertDirToUV(const izanagi::math::SVector3& dir)
+    static izanagi::math::SVector3 convertDirToUV(const izanagi::math::SVector3& dir)
     {
         auto temp = ::atan2(dir.x, dir.z);
         auto r = izanagi::math::SVector3::Length(dir);
@@ -63,7 +65,6 @@ public:
 
         // Map to [0,1]x[0,1] range and reverse Y axis
         auto u = phi / (2 * IZ_MATH_PI);
-        //auto v = 1 - theta / IZ_MATH_PI;
         auto v = theta / IZ_MATH_PI;
 
         izanagi::math::CVector3 uv(u, v, 0);
@@ -71,22 +72,21 @@ public:
         return std::move(uv);
     }
 
-    izanagi::math::SVector3 convertUVToDir(float u, float v)
+    static izanagi::math::SVector3 convertUVToDir(float u, float v)
     {
         // u = phi / 2PI
         // => phi = 2PI * u;
         auto phi = 2 * IZ_MATH_PI * u;
 
-        // v = 1 - theta / PI
-        // => theta = (1 - v) * PI;
-        //auto theta = (1 - v) * IZ_MATH_PI;
+        // v = theta / PI
+        // => theta = v * PI;
         auto theta = v * IZ_MATH_PI;
 
         izanagi::math::CVector3 dir;
 
         dir.y = ::cos(theta);
 
-        auto xz = ::sqrt(1 - dir.y * dir.y);
+        auto xz = ::sqrt(1 - dir.y * dir.y);    // sin(theta)
 
         dir.x = xz * ::sin(phi);
         dir.z = xz * ::cos(phi);
@@ -177,6 +177,7 @@ inline izanagi::math::SVector3 getOrthoVector(const izanagi::math::SVector3& n)
 }
 
 izanagi::math::SVector3 computeRadiance(
+    XorShift& sampler,
     EquirectTexture& in,
     const izanagi::math::CVector3& n,
     const izanagi::math::CVector3& t,
@@ -185,40 +186,35 @@ izanagi::math::SVector3 computeRadiance(
     // NOTE
     // http://www.codinglabs.net/article_physically_based_rendering.aspx
 
-    static const int step = 10;
-
-    static const float phi_step = IZ_MATH_PI2 / step;
-    static const float theta_step = IZ_MATH_PI1_2 / step;
-
-    int count = 0;
-
     izanagi::math::CVector3 conv;
 
     // normalの方向を基準とした正規直交基底(w, u, v)を作る.
     // この基底に対する半球内で次のレイを飛ばす.
 
-    for (float phi = 0.0f; phi < IZ_MATH_PI2; phi += phi_step) {
-        for (float theta = 0.0f; theta < IZ_MATH_PI1_2; theta += theta_step) {
-            float x = ::sin(theta) * ::sin(phi);
-            float y = ::sin(theta) * ::cos(phi);
-            float z = ::cos(theta);
+    // コサイン項を使った重点的サンプリング.
+    const float r1 = 2 * IZ_MATH_PI * sampler.nextSample();
+    const float r2 = sampler.nextSample();
+    const float r2s = sqrt(r2);
 
-            izanagi::math::CVector3 dir = t * x + b * y + n * z;
-            dir.Normalize();
+    const float x = ::cosf(r1) * r2s;
+    const float y = ::sinf(r1) * r2s;
+    const float z = ::sqrtf(1.0f - r2);
 
-            // テクスチャからサンプリング.
-            izanagi::math::CVector3 c(in.read(dir));
+    izanagi::math::CVector3 wo = t * x + b * y + n * z;
+    wo.Normalize();
 
-            conv += c * ::cos(theta) * ::sin(theta);
+    // テクスチャからサンプリング.
+    izanagi::math::CVector3 bsdf(in.read(wo));
+    bsdf /= IZ_MATH_PI;
 
-            count++;
-        }
-    }
+    auto c = izanagi::math::SVector3::Dot(n, wo);
+    c = ::fabsf(c);
 
-    // divide by samplings.
-    conv = IZ_MATH_PI * conv / (float)count;
-    
-    return std::move(conv);
+    auto pdf = c / IZ_MATH_PI;
+
+    auto ret = bsdf * c / pdf;
+
+    return std::move(ret);
 }
 
 inline izanagi::math::SVector3 cross(const izanagi::math::SVector3& v1, const izanagi::math::SVector3& v2)
@@ -232,14 +228,62 @@ inline izanagi::math::SVector3 cross(const izanagi::math::SVector3& v1, const iz
     return std::move(ret);
 }
 
+inline bool isValid(float f)
+{
+    bool b = isnan(f);
+    if (!b) {
+        b = isinf(f);
+    }
+
+    return !b;
+}
+
+inline bool isInvalid(const izanagi::math::SVector3& v)
+{
+    bool b0 = !isValid(v.x);
+    bool b1 = !isValid(v.y);
+    bool b2 = !isValid(v.z);
+
+    return b0 || b1 || b2;
+}
+
+inline bool isInvalidColor(const izanagi::math::SVector3& v)
+{
+    bool b = isInvalid(v);
+    if (!b) {
+        if (v.x < 0 || v.y < 0 || v.z < 0) {
+            b = true;
+        }
+    }
+
+    return b;
+}
+
 int main(int argc, char* argv[])
 {
     int nRetCode = 0;
 
     int width, height, comp;
-    auto data = stbi_loadf("studio015.hdr", &width, &height, &comp, 0);
+#if 1
+    //auto data = stbi_loadf("studio015.hdr", &width, &height, &comp, 0);
+    auto data = stbi_loadf("harbor.hdr", &width, &height, &comp, 0);
 
     EquirectTexture in(width, height, data);
+#else
+    auto data = stbi_load("input.png", &width, &height, &comp, 0);
+    float* f = (float*)malloc(width * height * 3 * sizeof(float));
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int idx = y * width + x;
+            idx *= 3;
+
+            f[idx + 0] = data[idx + 0] / (float)255;
+            f[idx + 1] = data[idx + 1] / (float)255;
+            f[idx + 2] = data[idx + 2] / (float)255;
+        }
+    }
+    EquirectTexture in(width, height, f);
+#endif
 
     int bytes = sizeof(float) * 3 * width * height;
     float* outdata = (float*)malloc(bytes);
@@ -248,27 +292,46 @@ int main(int argc, char* argv[])
 
     std::atomic<int> count = 0;
 
-//#pragma omp parallel for
+    static const int samples = 100;
+
+#pragma omp parallel for
     for (int y = 0; y < height; y++) {
         int cur = count.fetch_add(1);
         if ((cur % 10) == 0) {
-            IZ_PRINTF("%.3f %%\n", cur / (float)height * 100);
+           IZ_PRINTF("%.3f %%\n", cur / (float)height * 100);
         }
+
+        izanagi::math::CVector3 conv;
 
         for (int x = 0; x < width; x++) {
             float u = x / (float)(width - 1);
             float v = y / (float)(height - 1);
 
-            auto n = in.convertUVToDir(u, v);
+            auto n = EquirectTexture::convertUVToDir(u, v);
 
             auto t = getOrthoVector(n);
 
             auto b = cross(n, t);
 
-            //auto clr = in.read(n);
-            auto clr = computeRadiance(in, n, t, b);
+            int count = 0;
 
-            out.write(clr, n);
+            for (int i = 0; i < samples; i++) {
+                XorShift sampler((y * height * 4 + x * 4) * samples + i + 1);
+
+                auto clr = computeRadiance(sampler, in, n, t, b);
+
+                if (isInvalidColor(clr)) {
+                    IZ_PRINTF("Invalid(%d/%d[%d])\n", x, y, i);
+                    continue;
+                }
+
+                conv += clr;
+                count++;
+            }
+
+            conv /= (float)count;
+
+            out.write(conv, u, v);
         }
     }
 
---
+++ Tools/Image/IrradianceMapMaker/xorshift.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cstdint>
+#include <limits>
+
+// Xor-Shift による乱数ジェネレータ.
+class XorShift {
+public:
+	XorShift() {}
+	XorShift(const unsigned int initial_seed)
+	{
+		init(initial_seed);
+	}
+
+	virtual ~XorShift() {}
+
+	unsigned int next()
+	{
+		const unsigned int t = m_seed_[0] ^ (m_seed_[0] << 11);
+		m_seed_[0] = m_seed_[1];
+		m_seed_[1] = m_seed_[2];
+		m_seed_[2] = m_seed_[3];
+		return m_seed_[3] = (m_seed_[3] ^ (m_seed_[3] >> 19)) ^ (t ^ (t >> 8));
+	}
+
+	// [0, 1]
+	float nextSample()
+	{
+		//return (real)next() / (UINT_MAX + 1.0);
+        return (float)next() / UINT_MAX;
+	}
+
+	void init(uint32_t initial_seed)
+	{
+		unsigned int s = initial_seed;
+		for (int i = 1; i <= 4; i++) {
+			m_seed_[i - 1] = s = 1812433253U * (s ^ (s >> 30)) + i;
+		}
+	}
+
+private:
+	unsigned int m_seed_[4];
+};
