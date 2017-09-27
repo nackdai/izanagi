573b7bf338aaa6b248e43b936d5b6f8509f9ea3e
nakdai <newvf91.brjtn579.dn@gmail.com>
2017-09-27 18:18:57 +0900

    Add glfw as submodule for Tools.

--- Tools/BuildExternal.bat
+++ Tools/BuildExternal.bat
@@ -7,6 +7,9 @@ cd /d %~dp0
 set MSBUILD="C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe"
 set TARGET=Rebuild
 set CONFIG=%1
+set PLATFORM=Win32
+
+rem squish ===============================================================================
 
 if not exist "External\squish\build" (
     mkdir External\squish\build
@@ -18,6 +21,8 @@ if not exist "External\squish\build\squish.sln" (
     cd ..\..\..
 )
 
+rem assimp ===============================================================================
+
 if not exist "External\assimp\build" (
     mkdir External\assimp\build
 )
@@ -27,6 +32,28 @@ if not exist "External\assimp\build\Assimp.sln" (
     ..\..\..\..\External\cmake\bin\cmake.exe -G "Visual Studio 12 2013" ..\
     cd ..\..\..
 )
+
+rem glfw ================================================================================
+
+set BUILD_DIR=External\glfw\%PLATFORM%
+
+if not exist %BUILD_DIR% (
+    mkdir %BUILD_DIR%
+)
+
+if not exist %BUILD_DIR%\GLFW.sln (
+    cd %BUILD_DIR%
+    if %PLATFORM% == Win32 (
+        ..\..\..\..\External\cmake\bin\cmake.exe -D USE_MSVC_RUNTIME_LIBRARY_DLL=OFF -G "Visual Studio 12 2013" ..\
+    ) else (
+        ..\..\..\..\External\cmake\bin\cmake.exe -D USE_MSVC_RUNTIME_LIBRARY_DLL=OFF -G "Visual Studio 12 2013 Win64" ..\
+    )
+    cd ..\..\..\
+)
+
+%MSBUILD% %BUILD_DIR%\src\glfw.vcxproj /t:%TARGET% /p:Configuration=%CONFIG% /p:Platform=%PLATFORM% || goto error
+
+rem others ==============================================================================
 
 %MSBUILD% External\Preproc\Preproc.sln /t:%TARGET% /p:Configuration=%CONFIG% || goto error
 %MSBUILD% External\NvTriStrip\src\NvTriStrip\NvTriStrip.sln /t:%TARGET% /p:Configuration=%CONFIG% || goto error
