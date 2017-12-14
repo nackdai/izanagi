5214b879a40f8d62893dc2c3943bcb353e731415
nakdai <newvf91.brjtn579.dn@gmail.com>
2017-12-14 13:41:44 +0900

    Modify shader code.

--- Samples/BilateralUpsampling/shader/ps_final.glsl
+++ Samples/BilateralUpsampling/shader/ps_final.glsl
@@ -14,61 +14,94 @@ uniform vec4 invScreen;
 
 // NOTE
 // http://d.hatena.ne.jp/hanecci/20131030/p1
+// https://sites.google.com/site/monshonosuana/directxno-hanashi-1/directx-121
 // https://github.com/septag/darkhammer/blob/master/data/shaders/hlsl/upsample-bilateral.ps.hlsl
 
-const vec2 offsets[4] = vec2[4](
-	vec2( 0.0f,  1.0f),
-	vec2( 1.0f,  0.0f),
-	vec2( 0.0f, -1.0f),
-	vec2(-1.0f, 0.0f)
+const vec4 bilateralWeight[4] = vec4[4](
+	vec4(9.0 / 16.0, 3.0 / 16.0, 3.0 / 16.0, 1.0 / 16.0),
+	vec4(3.0 / 16.0, 9.0 / 16.0, 1.0 / 16.0, 3.0 / 16.0),
+	vec4(3.0 / 16.0, 1.0 / 16.0, 9.0 / 16.0, 3.0 / 16.0),
+	vec4(1.0 / 16.0, 3.0 / 16.0, 3.0 / 16.0, 9.0 / 16.0)
 );
 
 const float g_epsilon = 0.0001f;
 
+vec4 fetch(sampler2D tex, vec2 base, vec2 offset)
+{
+	vec2 uv = base + invScreen.xy * offset;
+	return texture2D(tex, uv);
+}
+
 void main()
 {
 	vec2 uv = gl_FragCoord.xy * invScreen.xy;
+	ivec2 pos = ivec2(gl_FragCoord.xy);
 
-	vec2 coords[4];
+	int idx = (1 - (pos.y & 0x01)) * 2 + (1 - (pos.x & 0x01));
 
-	for (int i = 0; i < 4; i++) {
-		coords[i] = uv + invScreen.xy * offsets[i];
+	vec4 lowResNmlDepth[4];
+	vec4 lowResClr[4];
+
+	switch (idx) {
+	case 0:
+		lowResNmlDepth[0] = fetch(s1, uv, vec2(0, 0));
+		lowResNmlDepth[1] = fetch(s1, uv, vec2(1, 0));
+		lowResNmlDepth[2] = fetch(s1, uv, vec2(0, 1));
+		lowResNmlDepth[3] = fetch(s1, uv, vec2(1, 1));
+		lowResClr[0] = fetch(s0, uv, vec2(0, 0));
+		lowResClr[1] = fetch(s0, uv, vec2(1, 0));
+		lowResClr[2] = fetch(s0, uv, vec2(0, 1));
+		lowResClr[3] = fetch(s0, uv, vec2(1, 1));
+		break;
+	case 1:
+		lowResNmlDepth[0] = fetch(s1, uv, vec2(-1, 0));
+		lowResNmlDepth[1] = fetch(s1, uv, vec2( 0, 0));
+		lowResNmlDepth[2] = fetch(s1, uv, vec2(-1, 1));
+		lowResNmlDepth[3] = fetch(s1, uv, vec2( 0, 1));
+		lowResClr[0] = fetch(s0, uv, vec2(-1, 0));
+		lowResClr[1] = fetch(s0, uv, vec2( 0, 0));
+		lowResClr[2] = fetch(s0, uv, vec2(-1, 1));
+		lowResClr[3] = fetch(s0, uv, vec2( 0, 1));
+		break;
+	case 2:
+		lowResNmlDepth[0] = fetch(s1, uv, vec2(0, -1));
+		lowResNmlDepth[1] = fetch(s1, uv, vec2(1, -1));
+		lowResNmlDepth[2] = fetch(s1, uv, vec2(0,  0));
+		lowResNmlDepth[3] = fetch(s1, uv, vec2(1,  0));
+		lowResClr[0] = fetch(s0, uv, vec2(0, -1));
+		lowResClr[1] = fetch(s0, uv, vec2(1, -1));
+		lowResClr[2] = fetch(s0, uv, vec2(0,  0));
+		lowResClr[3] = fetch(s0, uv, vec2(1,  0));
+		break;
+	case 3:
+		lowResNmlDepth[0] = fetch(s1, uv, vec2(-1, -1));
+		lowResNmlDepth[1] = fetch(s1, uv, vec2( 0, -1));
+		lowResNmlDepth[2] = fetch(s1, uv, vec2(-1,  0));
+		lowResNmlDepth[3] = fetch(s1, uv, vec2( 0,  0));
+		lowResClr[0] = fetch(s0, uv, vec2(-1, -1));
+		lowResClr[1] = fetch(s0, uv, vec2( 0, -1));
+		lowResClr[2] = fetch(s0, uv, vec2(-1,  0));
+		lowResClr[3] = fetch(s0, uv, vec2( 0,  0));
+		break;
 	}
 
-	// normal weight.
-
-	float nmlWeight[4];
-	
-	vec3 nmlHires = texture2D(s2, uv).xyz;
-
-	for (int i = 0; i < 4; i++) {
-		vec3 nmlCoarse = texture2D(s1, coords[i]).xyz;
-		nmlWeight[i] = pow(abs(dot(nmlCoarse, nmlHires)), 32);
-	}
-
-	// depth weight.
-
-	float depthWeight[4];
-
-	float depthHires = texture2D(s2, uv).w;
-
-	for (int i = 0; i < 4; i++) {
-		float depthCoars = texture2D(s1, coords[i]).w;
-		depthWeight[i] = 1.0f / (g_epsilon + abs(depthHires - depthCoars));
-	}
+	vec4 hiResNmlDepth = texture2D(s2, uv);
 
 	vec4 sum = vec4(0);
 	float weight = 0.0001f;
 
 	for (int i = 0; i < 4; i++) {
-		float w = nmlWeight[i] * depthWeight[i];
-		sum += texture2D(s0, coords[i]) * w;
+		float nmlWeight = clamp(pow(dot(hiResNmlDepth.xyz, lowResNmlDepth[i].xyz), 32), 0, 1);
+		float depthWeight = clamp(1.0 / (g_epsilon + abs(hiResNmlDepth.w - lowResNmlDepth[i].w)), 0, 1);
+
+		float w = nmlWeight * depthWeight * bilateralWeight[idx][i];
+		sum += lowResClr[i] * w;
 		weight += w;
 	}
 
 	sum /= weight;
 
-	if (depthHires <= 0.0f) {
+	if (hiResNmlDepth.w <= 0.0f) {
 		// Background.
 		outColor = texture2D(s0, uv);
 		outColor.a = 1.0f;
